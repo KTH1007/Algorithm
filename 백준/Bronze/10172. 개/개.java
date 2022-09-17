79ac831374abd18cd40b418feab601e680d8ca1d
KTH1007 <104629952+KTH1007@users.noreply.github.com>
2022-09-17 23:47:20 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 14040 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write("|\\_/|");
+		bw.write("\n");
+		bw.write("|q p|   /}");
+		bw.write("\n");
+		bw.write("( 0 )\"\"\"\\");
+		bw.write("\n");
+		bw.write("|\"^\"`    |");
+		bw.write("\n");
+		bw.write("||_/=\\\\__|");
+		bw.write("\n");
+		bw.flush();
+		bw.close();
+
+	}
+}
