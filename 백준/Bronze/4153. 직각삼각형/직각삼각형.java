9fe31f28a426474e55a7aa9beef8a22e276b3123
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2022-10-11 17:03:30 +0900

    [Bronze III] Title: 직각삼각형, Time: 120 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int[] arr = new int[3];
+			for(int i=0; i<3; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			if(arr[0] == 0 && arr[1] == 0 && arr[2] == 0) {
+				break;
+			}
+			Arrays.sort(arr);
+			if(Math.pow(arr[0], 2) + Math.pow(arr[1], 2) == Math.pow(arr[2], 2)) {
+				System.out.println("right");
+			}
+			else {
+				System.out.println("wrong");
+			}
+		}
+	}
+}
