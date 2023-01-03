6ffcd80f22b1c25da17c774d9ad6f0aa4b027b24
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-01-03 13:18:29 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.03 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -1,12 +1,14 @@
 class Solution {
     public int solution(int[][] lines) {
         int answer = 0;
-        int[] arr = new int[200];
+        int[] arr = new int[200]; //-100부터 100까지 길이를 저장하기 위한 배열
+        //-배열을 만들 수 없기 때문에 100을 더해줌
         for(int i=0; i<lines.length; i++){
             for(int j=lines[i][0]+100; j<lines[i][1]+100; j++){
                 arr[j]++;
             }
         }
+        //arr 배열의 값이 2이상이면 겹쳐진 선분임
         for(int i=0; i<arr.length; i++){
             if(arr[i] >=2)
                 answer++;
