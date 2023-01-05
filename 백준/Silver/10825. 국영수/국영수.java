93198cc9c3a733050c8127eadfd59027d901def0
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-01-05 12:37:21 +0900

    [Silver IV] Title: 국영수, Time: 1080 ms, Memory: 69072 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] temp;
+	static int result = -1;
+	static int count = 0;
+	static int k;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(st.nextToken());
+		String[][] arr = new String[n][4];
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			arr[i][0] = st.nextToken();
+			arr[i][1] = st.nextToken();
+			arr[i][2] = st.nextToken();
+			arr[i][3] = st.nextToken();
+		}
+		Arrays.sort(arr, new Comparator<String[]>() {
+			@Override
+			public int compare(String[] o1, String[] o2) {
+				if(o1[1].equals(o2[1])) {
+					if(o1[2].equals(o2[2])) {
+						if(o1[3].equals(o2[3])) {
+							return o1[0].compareTo(o2[0]);
+						}
+						return Integer.parseInt(o2[3]) - Integer.parseInt(o1[3]);
+					}
+					return Integer.parseInt(o1[2]) - Integer.parseInt(o2[2]);
+				}
+				return Integer.parseInt(o2[1]) - Integer.parseInt(o1[1]);
+			}
+		});
+		for(int i=0; i<n; i++) {
+			sb.append(arr[i][0]+"\n");
+		}
+		System.out.println(sb);
+	}
+}
