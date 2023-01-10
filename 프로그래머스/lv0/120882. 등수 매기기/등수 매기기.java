9cb907e936ca78660f577a58833a47c6bea9e1ad
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-01-10 14:44:36 +0900

    [level 0] Title: 등수 매기기, Time: 0.37 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -7,6 +7,7 @@ class Solution {
         }
         Collections.sort(list, Collections.reverseOrder());
         int[] answer = new int[score.length];
+        //indexOf를 사용하면 제일 작은 값이 나옴
         for(int i=0; i<answer.length; i++){
             answer[i] = list.indexOf(score[i][0] + score[i][1]) + 1;
         }
