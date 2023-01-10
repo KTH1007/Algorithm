c53dc5a7af10ed51c3d15ff8ec929da71d499e4f
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-01-10 14:43:22 +0900

    [level 0] Title: 등수 매기기, Time: 0.24 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[][] score) {
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<score.length; i++){
+            list.add(score[i][0] + score[i][1]);
+        }
+        Collections.sort(list, Collections.reverseOrder());
+        int[] answer = new int[score.length];
+        for(int i=0; i<answer.length; i++){
+            answer[i] = list.indexOf(score[i][0] + score[i][1]) + 1;
+        }
+        return answer;
+    }
+}
