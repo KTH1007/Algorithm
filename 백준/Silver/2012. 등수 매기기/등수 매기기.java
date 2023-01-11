01a01b0741e326404b89a5582c09ab9e501f83c6
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-01-11 15:02:19 +0900

    [Silver III] Title: 등수 매기기, Time: 856 ms, Memory: 50672 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		long count = 0;
+		for(int i=0; i<n; i++) {
+			count += Math.abs(arr[i] - (i+1));
+		}
+		System.out.println(count);
+	}
+}
