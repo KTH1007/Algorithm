27bb7247ba2dd430bafb0d20cfe1346116e41e2e
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-01-18 15:27:59 +0900

    [Silver III] Title: 두 수의 합, Time: 356 ms, Memory: 26932 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int x = Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
+        int count = 0;
+        int sum = 0;
+        int start = 0;
+        int end = n-1;
+        while (start < end){
+            sum = arr[start] + arr[end];
+            if(sum == x)
+                count++;
+            if(sum <= x)
+                start++;
+            else
+                end--;
+        }
+        System.out.println(count);
+    }
+}
