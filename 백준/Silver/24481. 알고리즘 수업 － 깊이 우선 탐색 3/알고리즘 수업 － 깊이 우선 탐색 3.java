7090515b9ddf6ef698e23b40496abcfe9aefbac7
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-01-30 18:12:40 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 1052 ms, Memory: 91848 KB -BaekjoonHub

---
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+   static List<List<Integer>> list = new ArrayList<>();
+   static int[] visited;
+   static int count = 1;
+   static int[] depth;
+   static int depthCount = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        int m= Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        for(int i=0; i<=n; i++){
+            list.add(new ArrayList<>());
+        }
+        for(int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list.get(a).add(b);
+            list.get(b).add(a);
+        }
+        for(int i=0; i<=n; i++){
+            Collections.sort(list.get(i));
+        }
+        visited = new int[n+1];
+        depth = new int[n+1];
+        Arrays.fill(depth, -1);
+        depth[r] = 0;
+        DFS(r);
+        for(int i=1; i<=n; i++){
+            sb.append(depth[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+    public static void DFS(int r){
+        if(visited[r] != 0)
+            return;
+        visited[r] = count++;
+        for(int i=0; i<list.get(r).size(); i++){
+            if(visited[list.get(r).get(i)] == 0){
+                depth[list.get(r).get(i)] = depth[r]+1;
+                DFS(list.get(r).get(i));
+            }
+        }
+    }
+}
