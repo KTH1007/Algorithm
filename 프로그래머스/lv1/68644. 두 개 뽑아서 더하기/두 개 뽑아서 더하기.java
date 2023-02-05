f3c6fe18a5c77924caac03ca9ead6dafaa9e35fa
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-02-05 23:25:57 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.44 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -13,18 +13,10 @@ class Solution {
             }
         }
         Collections.sort(list);
-        List<Integer> anslist = new ArrayList<>();
-        anslist.add(list.get(0));
-        for(int i=1; i<list.size(); i++){
-            if(list.get(i-1) == list.get(i)){
-                continue;
-            }
-            else
-                anslist.add(list.get(i));
-        }
-        int[] answer = new int[anslist.size()]; 
+
+        int[] answer = new int[list.size()]; 
         for(int i=0; i<answer.length; i++){
-            answer[i] = anslist.get(i);
+            answer[i] = list.get(i);
         }
         return answer;
     }
