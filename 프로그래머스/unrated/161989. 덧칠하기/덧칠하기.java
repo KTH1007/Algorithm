f9ed91d599e33355660551d7975d0a4a2357bcd8
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-03-28 20:30:42 +0900

    [unrated] Title: 덧칠하기, Time: 0.88 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int max = 0;
+        for(int i=0; i<section.length; i++){
+            if(section[i] < max)
+                continue;
+            answer += 1;
+            max = m + section[i];
+        }
+        
+        return answer;
+    }
+}
