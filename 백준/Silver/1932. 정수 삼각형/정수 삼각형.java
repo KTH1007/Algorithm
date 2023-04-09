a0829b6c2bf1ab27fb4370d654375c08a51fb194
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-04-09 14:03:39 +0900

    [Silver I] Title: 정수 삼각형, Time: 280 ms, Memory: 26940 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][n];
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<i+1; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int[][] dp = new int[n][n];
+        dp[0][0] = arr[0][0];
+        for(int i=1; i<n; i++){
+            for(int j=0; j<=i; j++){
+                if(j == 0){ //맨 왼쪽 열인 경우
+                    dp[i][j] = dp[i-1][j] + arr[i][j];
+                }
+                else if(j == i){ //맨 오른쪽 열인 경우
+                    dp[i][j] = dp[i-1][j-1] + arr[i][j];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + arr[i][j];
+                }
+            }
+        }
+        int max = 0;
+        for(int i=0; i<n; i++){
+            if(max < dp[n-1][i])
+                max = dp[n-1][i];
+        }
+        System.out.println(max);
+    }
+}
