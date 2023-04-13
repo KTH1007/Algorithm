0f40298c0e24b6ca015f4cb6cfdf9bf441d4087e
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-04-13 16:35:50 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 124 ms, Memory: 41540 KB -BaekjoonHub

---
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        Long[] dp = new Long[n+1];
+        if(n > 0){
+            dp[0] = Long.valueOf(0);
+        }
+        if(n> 1){
+            dp[0] = Long.valueOf(0);
+            dp[1] = Long.valueOf(1);
+        }
+        if(n>2){
+            dp[0] = Long.valueOf(0);
+            dp[1] = Long.valueOf(1);
+            dp[2] = Long.valueOf(1);
+        }
+
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        System.out.println(dp[n]);
+    }
+}
