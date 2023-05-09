114d545e2e8339816c7f885e4c881715cc75a260
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-05-09 10:37:47 +0900

    [Silver III] Title: N과 M (4), Time: 204 ms, Memory: 19396 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int m;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        DFS(0, 0);
+        System.out.println(sb);
+    }
+    static void DFS(int at, int depth){
+        if(m == depth){
+            for(int i=0; i<m; i++){
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i=at; i<n; i++){
+            arr[depth] = i+1;
+            DFS(i, depth+1);
+        }
+    }
+}
