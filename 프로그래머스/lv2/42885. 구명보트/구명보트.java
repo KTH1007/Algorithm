ad8b782d1bbcb7d64d426b87139f9464a97b34d4
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-05-11 14:14:36 +0900

    [level 2] Title: 구명보트, Time: 8.98 ms, Memory: 53.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int count = 0;
+        Arrays.sort(people);
+        for(int i=people.length-1; i>=count; i--){
+            if(people[i] + people[count] <= limit){
+                count++;
+                answer++;
+            }
+            else
+                answer++;
+        }
+        return answer;
+    }
+}
