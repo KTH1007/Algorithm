ba48e488f92b7b8cc936b94fbe45ea3db22e6685
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-05-24 11:44:55 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.11 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        long[] dp = new long[2001];
+        dp[0] = 1;
+        dp[1] = 2;
+        for(int i=2; i<n; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+        }
+        answer = dp[n-1];
+        return answer;
+    }
+}
