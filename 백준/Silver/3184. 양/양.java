79bf6629d9e48638e530d47a63e078fb8b6b2734
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-06-20 17:11:31 +0900

    [Silver I] Title: 양, Time: 228 ms, Memory: 20496 KB -BaekjoonHub

---
+++ 백준/Silver/3184. 양/양.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static char[][] arr;
+    static boolean[][] visited;
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,1,-1};
+    static int sheep = 0;
+    static int wolf = 0;
+    static int totalSheep = 0;
+    static int totalWolf = 0;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new char[n][m];
+        visited = new boolean[n][m];
+        for(int i=0; i<n; i++){
+            String s = br.readLine();
+            for(int j=0; j<m; j++){
+                arr[i][j] = s.charAt(j);
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(arr[i][j] != '#' && !visited[i][j]){
+                    BFS(i, j);
+                    if(wolf >= sheep)
+                        totalWolf += wolf;
+                    else totalSheep += sheep;
+                }
+            }
+        }
+        System.out.println(totalSheep + " " + totalWolf);
+    }
+    static void BFS(int x, int y){
+        wolf = 0;
+        sheep = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x,y});
+        visited[x][y] = true;
+        if(arr[x][y] == 'o')
+            sheep++;
+        else if(arr[x][y] == 'v')
+            wolf++;
+        while (!queue.isEmpty()){
+            int[] current = queue.poll();
+            for(int i=0; i<4; i++){
+                int nx = current[0] + dx[i];
+                int ny = current[1] + dy[i];
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny] && arr[nx][ny] != '#'){
+                    if(arr[nx][ny] == 'o')
+                        sheep++;
+                    else if(arr[nx][ny] == 'v')
+                        wolf++;
+                    visited[nx][ny] = true;
+                    queue.add(new int[]{nx,ny});
+                }
+            }
+        }
+    }
+}
