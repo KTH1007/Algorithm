e171d7bcbce9dc71ab1a3e6c6098204e435f139b
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-07-24 11:12:56 +0900

    [Gold V] Title: 토마토, Time: 632 ms, Memory: 119976 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static int[][] arr;
+    static int n, m;
+    static Queue<int[]> queue = new LinkedList<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if (arr[i][j] == 1) queue.add(new int[]{i, j});
+            }
+        }
+        BFS();
+        int day = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 0) {
+                    System.out.println(-1);
+                    return;
+                }
+                day = Math.max(day, arr[i][j]);
+            }
+        }
+        System.out.println(day - 1);
+
+    }
+
+    static void BFS() {
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && arr[nx][ny] == 0) {
+                    queue.add(new int[]{nx, ny});
+                    arr[nx][ny] = arr[x][y] + 1;
+                }
+            }
+        }
+    }
+
+}
+
