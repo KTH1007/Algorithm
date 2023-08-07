7667f0b72235d5b04c20416761be87ff5b24a224
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-08-07 11:15:07 +0900

    [Gold V] Title: A와 B, Time: 136 ms, Memory: 15784 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.java
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -1,32 +1,27 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int answer = 0;
-        String s1 = br.readLine();
-        String s2 = br.readLine();
-        int i=0;
-        while (s1.length() < s2.length()) {
-            if(s2.charAt(s2.length()-1) == 'A'){
-                s2 = s2.substring(0,s2.length()-1);
-            }
-            else if(s2.charAt(s2.length()-1) == 'B'){
-                s2 = s2.substring(0, s2.length()-1);
-                StringBuilder sb = new StringBuilder();
-                sb.append(s2);
-                sb.reverse();
-                s2 = sb.toString();
-            }
+    static String s, t;
 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        s = br.readLine();
+        t = br.readLine();
+        while (t.length() > s.length()) {
+            if (t.charAt(t.length() - 1) == 'A') {
+                t = t.substring(0, t.length() - 1);
+            } else if (t.charAt(t.length() - 1) == 'B') {
+                t = t.substring(0, t.length() - 1);
+                StringBuilder sb = new StringBuilder();
+                sb.append(t).reverse();
+                t = sb.toString();
+            }
         }
-        if(s1.equals(s2))
-            answer = 1;
-        else answer = 0;
-        //System.out.println(s1);
-        System.out.println(answer);
+        if (t.equals(s)) System.out.println(1);
+        else System.out.println(0);
     }
+
+
 }
+
