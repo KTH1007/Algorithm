16a204a1977c6b8178ac642794123b9f8461ec87
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-08-30 11:06:38 +0900

    [Silver II] Title: N과 M (9), Time: 196 ms, Memory: 20864 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,16 +1,16 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[] visited;
+
+    static int n, m;
     static int[] arr;
-    static int n;
-    static int m;
+    static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
@@ -18,32 +18,33 @@ public class Main {
         arr = new int[n];
         visited = new boolean[n];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
-        int[] arr2 = new int[arr.length];
-        DFS(0, arr2);
+        int[] arr2 = new int[n];
+        dfs(0, arr2);
         System.out.println(sb);
     }
 
-    static void DFS(int depth, int[] arr2){
-        if(depth == m){
-            for(int i=0; i<m; i++){
+    static void dfs(int depth, int[] arr2) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
                 sb.append(arr2[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
         int before = 0;
-        for(int i=0; i<n; i++){
-            if(visited[i] || before == arr[i])
-                continue;
+        for (int i = 0; i < n; i++) {
+            if (visited[i] || before == arr[i]) continue;
             visited[i] = true;
             arr2[depth] = arr[i];
             before = arr[i];
-            DFS(depth+1, arr2);
+            dfs(depth + 1, arr2);
             visited[i] = false;
         }
     }
+
+
 }
