44e85142f7241ed8418d2819891c1eb5ed2b3d56
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-09-04 11:27:31 +0900

    [Gold IV] Title: 도서관, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Gold/1461. 도서관/도서관.java
+++ 백준/Gold/1461. 도서관/도서관.java
@@ -1,49 +1,41 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    static int n, m;
+    static List<Integer> plus = new ArrayList<>();
+    static List<Integer> minus = new ArrayList<>();
+    static int max = Integer.MIN_VALUE;
+    static int count = 0;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        List<Integer> plus = new ArrayList<>();
-        List<Integer> minus = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        int max = 0;
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(st.nextToken());
-            if(Math.abs(num) > max){
-                max = Math.abs(num);
-            }
-            if(num> 0)
-                plus.add(num);
+            if (Math.abs(num) > max) max = Math.abs(num);
+            if (num > 0) plus.add(num);
             else minus.add(Math.abs(num));
         }
-        int count = 0;
         Collections.sort(plus, Collections.reverseOrder());
         Collections.sort(minus, Collections.reverseOrder());
-        for(int i=0; i<plus.size(); i++){
-            if(plus.get(i) == max && i%m == 0){
-                count += plus.get(i);
-            }
-            else if(i % m == 0)
-                count += (plus.get(i) * 2);
-        }
 
-        for(int i=0; i<minus.size(); i++){
-            if(minus.get(i) == max && i%m == 0){
-                count += minus.get(i);
-            }
-            else if(i % m == 0){
-                count += (minus.get(i) * 2);
-            }
+        for (int i = 0; i < plus.size(); i++) {
+            if (plus.get(i) == max && i % m == 0) count += plus.get(i);
+            else if (i % m == 0) count += (plus.get(i) * 2);
+        }
+        for (int i = 0; i < minus.size(); i++) {
+            if (minus.get(i) == max && i % m == 0) count += minus.get(i);
+            else if (i % m == 0) count += (minus.get(i) * 2);
         }
         System.out.println(count);
+
     }
+
 
 }
