6b1058acb3558624176c3768bef336fe2db34ff0
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-09-05 15:42:27 +0900

    [Gold V] Title: 트리, Time: 132 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -1,50 +1,50 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
+
+    static int n, delete, count = 0;
     static int[] node;
-    static int count = 0, delete;
     static boolean[] visited;
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
         node = new int[n];
+        visited = new boolean[n];
         int root = 0;
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             node[i] = Integer.parseInt(st.nextToken());
-            if(node[i] == -1) root = i;
+            if (node[i] == -1) root = i;
         }
         delete = Integer.parseInt(br.readLine());
         deleteNode(delete);
-        visited = new boolean[n];
         countLeaf(root);
         System.out.println(count);
     }
-    static void deleteNode(int d){
-        node[d] = -2;  //삭제된 노드는 -2로 표시
-        for(int i=0; i<n; i++){
-            if(node[i] == d){
-                deleteNode(i);
-            }
+
+    static void deleteNode(int d) {
+        node[d] = -2;
+        for (int i = 0; i < n; i++) {
+            if (node[i] == d) deleteNode(i);
         }
     }
-    static void countLeaf(int s){
+
+    static void countLeaf(int c) {
         boolean isLeaf = true;
-        visited[s] = true;
-        if(node[s] != -2){
-            for(int i=0; i<n; i++){
-                if(node[i] == s && !visited[i]){
+        visited[c] = true;
+        if (node[c] != -2) {
+            for (int i = 0; i < n; i++) {
+                if (node[i] == c && !visited[i]) {
                     countLeaf(i);
                     isLeaf = false;
                 }
             }
-            if(isLeaf) count++;
+            if (isLeaf) count++;
         }
     }
+
 
 }
