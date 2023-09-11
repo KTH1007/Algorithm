b8e57e458c03d0df16e01a5f5fdeff7d8578f9d7
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-09-11 13:53:14 +0900

    [Silver III] Title: 피보나치는 지겨웡~, Time: 128 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Silver/17175. 피보나치는 지겨웡～/피보나치는 지겨웡～.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if (n < 2) {
+            System.out.println(1);
+            return;
+        }
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1] + 1) % 1000000007;
+        }
+        System.out.println(dp[n]);
+    }
+
+
+}
