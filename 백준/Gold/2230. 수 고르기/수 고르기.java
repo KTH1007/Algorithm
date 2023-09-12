27ad7a3626105f5bf95934a3916a49d156e82140
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-09-12 11:16:33 +0900

    [Gold V] Title: 수 고르기, Time: 356 ms, Memory: 27508 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -1,36 +1,38 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int[] arr = new int[n];
-        for(int i=0; i<n; i++){
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
         Arrays.sort(arr);
-        int i = 0;
-        int j = 0;
-        int answer = Integer.MAX_VALUE;
-        while(i<n){
-            if((arr[i] - arr[j]) < m){
+        int i = 0, j = 0;
+        while (i < n) {
+            int temp = arr[i] - arr[j];
+            if (temp < m) {
                 i++;
                 continue;
             }
-            if((arr[i] - arr[j]) == m){
-                answer = m;
+            if (temp == m) {
+                answer = temp;
                 break;
             }
-            answer = Math.min(answer,arr[i]-arr[j]);
+            answer = Math.min(answer, temp);
             j++;
         }
         System.out.println(answer);
     }
+
+
 }
