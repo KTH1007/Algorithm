d5df89d106b944552589c81044d1a7eda1e49455
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-09-18 10:32:03 +0900

    [Silver II] Title: 수익, Time: 460 ms, Memory: 84232 KB -BaekjoonHub

---
+++ 백준/Silver/4097. 수익/수익.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) break;
+            int max = Integer.MIN_VALUE;
+            int[] arr = new int[n];
+            int[] dp = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(br.readLine());
+                if (i != 0 && arr[i] + arr[i - 1] > arr[i]) {
+                    arr[i] += arr[i - 1];
+                }
+                max = Math.max(max, arr[i]);
+            }
+            sb.append(max).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+
+}
