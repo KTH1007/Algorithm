0bbecf838ac286df65e05a87b542f3b26f825872
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-09-18 10:53:55 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 120 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,27 +1,26 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
-        String s = st.nextToken();
-        int sum = Integer.MAX_VALUE;
-        String[] sarr = s.split("-");
-        for(int i=0; i<sarr.length; i++){
-            String[] plus = sarr[i].split("\\+");
-            int temp = 0;
-            for(int j=0; j<plus.length; j++){
-                temp += Integer.parseInt(plus[j]);
-            }
-            if(sum == Integer.MAX_VALUE){ //첫 번째 배열은 무조건 양수이기 때문에 sum에 대입해줌
-                sum = temp;
-            }
-            else sum -= temp;
+        String s = br.readLine();
+        String[] arr = s.split("-");
+        int plus = 0;
+        String[] temp = arr[0].split("\\+");
+        for (int i = 0; i < temp.length; i++) {
+            plus += Integer.parseInt(temp[i]);
         }
-        System.out.println(sum);
+        int minus = 0;
+        for (int i = 1; i < arr.length; i++) {
+            temp = arr[i].split("\\+");
+            for (int j = 0; j < temp.length; j++) {
+                minus += Integer.parseInt(temp[j]);
+            }
+        }
+        System.out.println(plus - minus);
     }
+
+
 }
