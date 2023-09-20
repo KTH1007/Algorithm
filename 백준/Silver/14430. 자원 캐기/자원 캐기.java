98f569b1ccd42644307ef9ec2e28e506edfdd2a6
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-09-20 10:55:29 +0900

    [Silver II] Title: 자원 캐기, Time: 240 ms, Memory: 22756 KB -BaekjoonHub

---
+++ 백준/Silver/14430. 자원 캐기/자원 캐기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                //현재값 기준 왼쪽과 위쪽 값 중에 최대값을 가져와서 더해줌
+                dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]) + arr[i][j];
+            }
+        }
+        System.out.println(dp[n][m]);
+
+    }
+
+
+}
