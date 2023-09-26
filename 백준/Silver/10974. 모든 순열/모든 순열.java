ce3bfcb38afa78de5220a2ae654983f163045507
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-09-26 15:51:34 +0900

    [Silver III] Title: 모든 순열, Time: 320 ms, Memory: 34180 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.java
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -1,38 +1,37 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    static boolean[] visited;
-    static int[] arr;
     static int n;
+    static int arr[];
+    static boolean visited[];
     static StringBuilder sb = new StringBuilder();
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-
-        visited = new boolean[n];
         arr = new int[n];
+        visited = new boolean[n];
         DFS(0);
         System.out.println(sb);
-    }
 
-    static void DFS(int depth) {
-        if (depth == n) {
-            for (int i = 0; i < n; i++) {
-                sb.append(arr[i]).append(" ");
+    }
+    static void DFS(int depth){
+        if(depth == n){
+            for(int i=0; i<n; i++){
+                sb.append(arr[i] + " ");
             }
             sb.append("\n");
             return;
         }
-        for (int i = 0; i < n; i++) {
-            if (visited[i]) continue;
+        for(int i=0; i<n; i++){
+            if(visited[i])
+                continue;
+            arr[depth] = i+1;
             visited[i] = true;
-            arr[depth] = i + 1;
-            DFS(depth + 1);
+            DFS(depth+1);
             visited[i] = false;
         }
-
     }
-
 }
