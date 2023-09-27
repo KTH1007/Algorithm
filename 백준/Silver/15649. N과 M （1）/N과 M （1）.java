fd6e9fd7299035bed3e799fa60e19176c9244a0a
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-09-27 11:14:51 +0900

    [Silver III] Title: N과 M (1), Time: 312 ms, Memory: 33972 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,41 +1,37 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static int[] arr;
     static boolean[] visited;
-    static int n, m;
     static StringBuilder sb = new StringBuilder();
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
         arr = new int[m];
-        visited = new boolean[n + 1];
-        DFS(0);
+        visited = new boolean[n];
+        DFS(n,m,0);
         System.out.println(sb);
     }
-
-    static void DFS(int depth) {
-        if (depth == m) {
-            for (int i = 0; i < m; i++) {
-                sb.append(arr[i]).append(" ");
+    static void DFS(int n, int m, int depth){
+        if(depth == m){
+            for(int i=0; i<m; i++){
+                sb.append(arr[i] + " ");
             }
             sb.append("\n");
             return;
         }
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                arr[depth] = i + 1;
-                DFS(depth + 1);
-                visited[i] = false;
-            }
+        for(int i=0; i<n; i++){
+            if(visited[i])
+                continue;
+            arr[depth] = i+1;
+            visited[i] = true;
+            DFS(n,m,depth+1);
+            visited[i] = false;
         }
-
     }
-
 }
