a12b36823109652d7cfb21509aea404f704c4b75
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-10-10 11:18:00 +0900

    [Silver II] Title: 로또, Time: 136 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,44 +1,46 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int n;
     static int[] arr;
     static boolean[] visited;
-    static int size;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        while(true){
-            st = new StringTokenizer(br.readLine());
-            size = Integer.parseInt(st.nextToken());
-            if(size == 0)
-                break;
-            arr = new int[size];
-            visited = new boolean[size];
-            for(int i=0; i<size; i++){
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            if (n == 0) break;
+            arr = new int[n];
+            visited = new boolean[n];
+            for (int i = 0; i < n; i++) {
                 arr[i] = Integer.parseInt(st.nextToken());
             }
-            DFS(0, 0);
+            dfs(0, 0);
             sb.append("\n");
         }
         System.out.println(sb);
     }
-    static void DFS(int depth, int start){
-        if(depth == 6){
-            for(int i=0; i<size; i++){
-                if(visited[i])
-                    sb.append(arr[i] + " ");
+
+    static void dfs(int depth, int start) {
+        if (depth == 6) {
+            for (int i = 0; i < n; i++) {
+                if (visited[i]) sb.append(arr[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
-        for(int i=start; i<size; i++){
+        for (int i = start; i < n; i++) {
             visited[i] = true;
-            DFS(depth+1, i+1);
+            dfs(depth + 1, i + 1);
             visited[i] = false;
         }
     }
 }
+
+
+
+
