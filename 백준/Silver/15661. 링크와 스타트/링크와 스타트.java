b3b0d24926868b6a267e10bb74c07c35d800fd3f
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-10-20 11:14:24 +0900

    [Silver I] Title: 링크와 스타트, Time: 1104 ms, Memory: 19072 KB -BaekjoonHub

--- 백준/Silver/15661. 링크와 스타트/링크와 스타트.java
+++ 백준/Silver/15661. 링크와 스타트/링크와 스타트.java
@@ -1,55 +1,57 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[] visited;
-    static int[][] arr;
     static int n;
-    static int answer = Integer.MAX_VALUE;
-
+    static int[][] arr;
+    static int min = Integer.MAX_VALUE;
+    static boolean[] visited;
     static int t = 0;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        visited = new boolean[n];
+        n = Integer.parseInt(br.readLine());
         arr = new int[n][n];
-        for(int i=0; i<n; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<n; j++){
+        visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for(t=1; t<n; t++){
-            DFS(0,0);
+        for (t = 1; t < n; t++) {
+            dfs(0, 0);
         }
-        System.out.println(answer);
+
+        System.out.println(min);
     }
-    static void DFS(int depth, int idx){
-        if(depth == t){
-            int start = 0;
-            int link = 0;
-            for(int i=0; i<n-1; i++){
-                for(int j=i+1; j<n; j++){
-                    if(visited[i] && visited[j])
-                        start += arr[i][j] + arr[j][i];
-                    if(!visited[i] && !visited[j])
-                        link += arr[i][j] + arr[j][i];
-                }
-            }
-            answer = Math.min(answer, Math.abs(start - link));
+
+    static void dfs(int depth, int index) {
+        if (depth == t) {
+            diff();
+            return;
         }
-        for(int i=idx; i<n; i++){
-            if(visited[i])
-                continue;
+        for (int i = index; i < n; i++) {
+            if (visited[i]) continue;
             visited[i] = true;
-            DFS(depth+1, i+1);
+            dfs(depth + 1, i + 1);
             visited[i] = false;
         }
+    }
+
+    static void diff() {
+        int start = 0;
+        int link = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (visited[i] && visited[j]) start += arr[i][j] + arr[j][i];
+                else if (!visited[i] && !visited[j]) link += arr[i][j] + arr[j][i];
+            }
+        }
+        min = Math.min(min, Math.abs(start - link));
     }
 
 
