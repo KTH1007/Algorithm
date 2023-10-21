c58de6e1e4b45eb18013dcbb6f12863c24b3e7ca
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-10-21 15:49:55 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    static int n;
+    static List<Long> list = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+
+
+}
