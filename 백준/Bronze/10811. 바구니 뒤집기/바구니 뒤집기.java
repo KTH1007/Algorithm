6a24dcae8cd58b6543470fd5c6c2d22d7c5edc41
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-10-23 09:29:35 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 148 ms, Memory: 15888 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,20 +1,17 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static List<Long> list = new ArrayList<>();
+
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
         int[] arr = new int[n + 1];
+
         for (int i = 1; i <= n; i++)
             arr[i] = i;
 
@@ -22,18 +19,17 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            for (int j = a; j <= b; j++) {
-                for (int k = j + 1; k <= b; k++) {
-                    int temp = arr[j];
-                    arr[j] = arr[k];
-                    arr[k] = temp;
-                }
+
+            while (a < b) {
+                int temp = arr[a];
+                arr[a++] = arr[b];
+                arr[b--] = temp;
             }
         }
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++)
             System.out.print(arr[i] + " ");
-        }
+
     }
 
 
