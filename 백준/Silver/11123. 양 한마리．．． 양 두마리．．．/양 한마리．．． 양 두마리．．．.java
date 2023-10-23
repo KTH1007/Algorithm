91155f9132f999b2ef12283c12d0e180391fd565
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-10-23 13:07:40 +0900

    [Silver II] Title: 양 한마리... 양 두마리..., Time: 248 ms, Memory: 26624 KB -BaekjoonHub

--- 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.java
+++ 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.java
@@ -1,52 +1,68 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,1,-1};
-    public static void main(String[] args) throws Exception{
+    static int h, w;
+    static char[][] arr;
+    static int count;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+    static boolean[][] visited;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
-        while (t-- >0){
+        while (t-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int h = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            char[][] arr = new char[h][w];
-            boolean[][] visited = new boolean[h][w];
-            for(int i=0; i<h; i++){
+            h = Integer.parseInt(st.nextToken());
+            w = Integer.parseInt(st.nextToken());
+            arr = new char[h][w];
+            visited = new boolean[h][w];
+            count = 0;
+            for (int i = 0; i < h; i++) {
                 String s = br.readLine();
-                for(int j=0; j<w; j++){
+                for (int j = 0; j < w; j++) {
                     arr[i][j] = s.charAt(j);
                 }
             }
-            BFS(h,w,arr,visited);
 
-        }
-    }
-    static void BFS(int h, int w, char[][] arr, boolean[][] visited){
-        int result = 0;
-        Queue<int[]> queue = new LinkedList<>();
-        for(int i=0; i<h; i++){
-            for(int j=0; j<w; j++){
-                if(arr[i][j] == '#' && !visited[i][j]){
-                    result++;
-                    queue.add(new int[]{i,j});
-                    visited[i][j] = true;
-                }
-                while (!queue.isEmpty()){
-                    int[] a = queue.poll();
-                    for(int k=0; k<4; k++){
-                        int nx = a[0] + dx[k];
-                        int ny = a[1] + dy[k];
-                        if(nx >= 0 && nx < h && ny >= 0 && ny < w && arr[nx][ny] == '#' && !visited[nx][ny]){
-                            queue.add(new int[]{nx, ny});
-                            visited[nx][ny] = true;
-                        }
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (!visited[i][j] && arr[i][j] == '#') {
+                        count++;
+                        bfs(i, j);
                     }
                 }
             }
+            sb.append(count).append("\n");
         }
-        System.out.println(result);
+
+        System.out.println(sb);
+
     }
+
+    static void bfs(int a, int b) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{a, b});
+        visited[a][b] = true;
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx >= 0 && nx < h && ny >= 0 && ny < w && arr[nx][ny] == '#' && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    queue.add(new int[]{nx, ny});
+                }
+            }
+        }
+    }
+
+
 }
