b5e9fee154c1ae09c4bbb3a318b74159cecc78c8
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-10-23 09:53:44 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 3, Time: 1048 ms, Memory: 90352 KB -BaekjoonHub

--- 백준/Silver/24446. 알고리즘 수업 － 너비 우선 탐색 3/알고리즘 수업 － 너비 우선 탐색 3.java
+++ 백준/Silver/24446. 알고리즘 수업 － 너비 우선 탐색 3/알고리즘 수업 － 너비 우선 탐색 3.java
@@ -1,56 +1,61 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.DatagramPacket;
 import java.util.*;
 
 public class Main {
+    static int n, m, r;
     static List<List<Integer>> list = new ArrayList<>();
     static boolean[] visited;
-    static int n,m,r;
-    static StringBuilder sb = new StringBuilder();
-    static int[] depth;
-    public static void main(String[] args) throws Exception{
+    static int[] arr;
+    static int count = 0;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         r = Integer.parseInt(st.nextToken());
-        for(int i=0; i<=n; i++){
+
+        for (int i = 0; i <= n; i++) {
             list.add(new ArrayList<>());
         }
-        for(int i=0; i<m; i++){
+
+        visited = new boolean[n + 1];
+        arr = new int[n + 1];
+        Arrays.fill(arr, -1);
+        arr[r] = 0;
+
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             list.get(a).add(b);
             list.get(b).add(a);
         }
-        for(int i=1; i<=n; i++){
+        for (int i = 1; i <= n; i++) {
             Collections.sort(list.get(i));
         }
-        depth = new int[n+1];
-        visited = new boolean[n+1];
-        Arrays.fill(depth, -1);
-        depth[r] = 0;
-        BFS(r);
-        for(int i=1; i<=n; i++){
-            sb.append(depth[i]).append("\n");
+
+        bfs(r);
+        for (int i = 1; i <= n; i++) {
+            sb.append(arr[i]).append("\n");
         }
         System.out.println(sb);
     }
-    static void BFS(int r){
+
+    static void bfs(int r) {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(r);
         visited[r] = true;
-        while (!queue.isEmpty()){
-            int a = queue.poll();
-            for(int i=0; i<list.get(a).size(); i++){
-                int next = list.get(a).get(i);
-                if(visited[next])
-                    continue;
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            for (int i = 0; i < list.get(current).size(); i++) {
+                int next = list.get(current).get(i);
+                if (visited[next]) continue;
                 visited[next] = true;
                 queue.add(next);
-                depth[next] = depth[a] + 1;
+                arr[next] = arr[current] + 1;
             }
         }
     }
