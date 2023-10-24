79fe7ca4411a0fc6cb08d3927f621727fb5c6cf0
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-10-24 09:53:38 +0900

    [Silver II] Title: 침투, Time: 448 ms, Memory: 44036 KB -BaekjoonHub

--- 백준/Silver/13565. 침투/침투.java
+++ 백준/Silver/13565. 침투/침투.java
@@ -1,50 +1,55 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
-    static int n, m;
-    static int[][] arr;
+    static int m, n;
+    static char[][] arr;
     static boolean[][] visited;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
     static boolean flag = false;
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
-        arr = new int[m][n];
+        arr = new char[m][n];
         visited = new boolean[m][n];
-        for(int i=0; i<m; i++){
+
+        for (int i = 0; i < m; i++) {
             String s = br.readLine();
-            for(int j=0; j<n; j++){
-                arr[i][j] = s.charAt(j) - '0';
-            }
+            for (int j = 0; j < n; j++)
+                arr[i][j] = s.charAt(j);
         }
-        for(int i=0; i<n; i++){
-            if(flag) break;
-            if(arr[0][i] == 0){
-                visited = new boolean[m][n];
-                visited[0][i] = true;
-                DFS(0,i);
-            }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[0][i] == '0' && !flag) bfs(i);
         }
-        if(flag) System.out.println("YES");
+
+        if (flag) System.out.println("YES");
         else System.out.println("NO");
     }
-    static void DFS(int x, int y){
-        if(x == m-1){
-            flag = true;
-            return;
-        }
-        for(int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(nx >= 0 && nx < m && ny >= 0 && ny < n && arr[nx][ny] == 0 && !visited[nx][ny]){
-                visited[nx][ny] = true;
-                DFS(nx, ny);
+
+    static void bfs(int start) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, start});
+        visited[0][start] = true;
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = current[0] + dx[i];
+                int ny = current[1] + dy[i];
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny] && arr[nx][ny] == '0') {
+                    if (nx == m - 1) flag = true;
+                    visited[nx][ny] = true;
+                    queue.add(new int[]{nx, ny});
+                }
             }
         }
     }
+
 }
