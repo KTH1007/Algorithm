9b51a2e875807d1a00b94e48066c480b3b1bbaca
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-10-24 13:31:44 +0900

    [Silver II] Title: 아기 상어 2, Time: 468 ms, Memory: 198504 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
@@ -1,55 +1,65 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n,m;
+    static int n, m;
     static int[][] arr;
+    static int[] dx = {-1, 0, 1, -1, 1, -1, 0, 1};
+    static int[] dy = {1, 1, 1, 0, 0, -1, -1, -1};
     static boolean[][] visited;
-    static int[][] move = {{1,1},{1,0},{1,-1},{0,1},{0,-1},{-1,0},{-1,-1},{-1,1}};
-    static int count = 0;
-    public static void main(String[] args) throws Exception{
+    static int max = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+
         arr = new int[n][m];
         visited = new boolean[n][m];
-        for(int i=0; i<n; i++){
+
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<m; j++){
+            for (int j = 0; j < m; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(arr[i][j] == 1) continue;
-                int temp = BFS(i,j);
-                count = Math.max(count, temp);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 0) bfs(i, j);
             }
         }
-        System.out.println(count);
 
+        System.out.println(max);
     }
-    static int BFS(int row, int col){
+
+    static void bfs(int a, int b) {
         visited = new boolean[n][m];
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{row,col,0});
-        visited[row][col] = true;
-        while (!queue.isEmpty()){
+        queue.add(new int[]{a, b, 0});
+        visited[a][b] = true;
+        while (!queue.isEmpty()) {
             int[] current = queue.poll();
-            for(int i=0; i<8; i++){
-                int nx = current[0] + move[i][0];
-                int ny = current[1] + move[i][1];
-                int dist = current[2]+1;
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny])
-                    continue;
-                if(arr[nx][ny] == 1)
-                    return dist;
-                queue.add(new int[]{nx,ny,dist});
-                visited[nx][ny] = true;
+            for (int i = 0; i < 8; i++) {
+                int nx = current[0] + dx[i];
+                int ny = current[1] + dy[i];
+                int dist = current[2] + 1;
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny]) {
+                    if (arr[nx][ny] == 1) {
+                        max = Math.max(max, dist);
+                        return;
+                    }
+                    visited[nx][ny] = true;
+                    queue.add(new int[]{nx, ny, dist});
+
+                }
             }
         }
-        return 0;
     }
+
+
 }
