772442a78e3813c31a4f37e9b592c85cc1dc1d27
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-10-25 15:13:18 +0900

    [Silver I] Title: 효율적인 해킹, Time: 8116 ms, Memory: 304756 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -3,52 +3,54 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int n,m;
+    static int n, m;
     static boolean[] visited;
     static List<List<Integer>> list = new ArrayList<>();
     static int max = Integer.MIN_VALUE;
     static int count;
-    public static void main(String[] args) throws Exception{
+    static int[] arr;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        for(int i=0; i<=n; i++){
+        for (int i = 0; i <= n; i++) {
             list.add(new ArrayList<>());
         }
-        for(int i=0; i<m; i++){
+
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            list.get(b).add(a);
+            list.get(a).add(b);
         }
-        int[] result = new int[n+1];
-        for(int i=1; i<=n; i++){
-            visited = new boolean[n+1];
-            count = 0;
+
+        arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            visited = new boolean[n + 1];
             BFS(i);
-            result[i] = count;
-            max = Math.max(max, count);
         }
-        for(int i=1; i<=n; i++){
-            if(result[i] == max)
-                sb.append(i + " ");
+        for (int i = 1; i <= n; i++)
+            max = Math.max(max, arr[i]);
+        for (int i = 1; i <= n; i++) {
+            if (arr[i] == max) sb.append(i + " ");
         }
         System.out.println(sb);
     }
-    static void BFS(int v){
+
+    static void BFS(int v) {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(v);
         visited[v] = true;
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int current = queue.poll();
-            for(int i : list.get(current)){
-                if(visited[i])
-                    continue;
-                visited[i] = true;
-                queue.add(i);
-                count++;
+            for (int next : list.get(current)) {
+                if (visited[next]) continue;
+                visited[next] = true;
+                queue.add(next);
+                arr[next]++;
             }
         }
     }
