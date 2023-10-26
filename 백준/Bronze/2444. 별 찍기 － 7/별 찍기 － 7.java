54c34408de085b3b4017b3b656f1798d028df396
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-10-26 09:29:16 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 492 ms, Memory: 21184 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            for (int j = n - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i + i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = n - i; j > 0; j--)
+                System.out.print(" ");
+            for (int j = 1; j <= i + i - 1; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+
+}
