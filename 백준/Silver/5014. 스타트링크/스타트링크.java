902a39b5ae6d9bce1e3719f24c95618f3b74121e
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-10-31 14:17:18 +0900

    [Silver I] Title: 스타트링크, Time: 204 ms, Memory: 62160 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.java
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -1,43 +1,55 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int f,s,g,u,d;
-    static int count = 0;
-    static int[] visited;
-    public static void main(String[] args) throws Exception{
+    static int f, s, g, u, d;
+    static int result = 0;
+    static boolean[] visited = new boolean[1000001];
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        f = Integer.parseInt(st.nextToken());  // 층 수
-        s = Integer.parseInt(st.nextToken());  // 현재위치
-        g = Integer.parseInt(st.nextToken());  // 목표 층
+        f = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+        g = Integer.parseInt(st.nextToken());
         u = Integer.parseInt(st.nextToken());
         d = Integer.parseInt(st.nextToken());
-        visited = new int[f+1];
-        count = BFS();
-        if(count != -1)
-            System.out.println(count);
-        else System.out.println("use the stairs");
-    }
-    static int BFS(){
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(s);
-        visited[s] = 1;
-        while (!queue.isEmpty()){
-            int current = queue.poll();
-            if(current == g)
-                return visited[current] - 1;
-            if(current + u <= f && visited[current+u] == 0){
-                visited[current+u] = visited[current]+1;
-                queue.add(current+u);
-            }
-            if(current-d > 0 && visited[current-d] == 0){
-                visited[current-d] = visited[current] + 1;
-                queue.add(current-d);
-            }
 
+        if (s == g) System.out.println(0);
+        else {
+            bfs();
+            if (result != 0) System.out.println(result);
+            else System.out.println("use the stairs");
         }
-        return -1;
+
+
     }
+
+    static void bfs() {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{s, 0});
+        visited[s] = true;
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int floor = current[0];
+            int count = current[1];
+            if (floor == g) {
+                result = count;
+                return;
+            }
+            if (floor + u <= f && !visited[floor + u]) {
+                visited[floor + u] = true;
+                queue.add(new int[]{floor + u, count + 1});
+            }
+            if (floor - d >= 1 && !visited[floor - d]) {
+                visited[floor - d] = true;
+                queue.add(new int[]{floor - d, count + 1});
+            }
+        }
+    }
+
+
 }
