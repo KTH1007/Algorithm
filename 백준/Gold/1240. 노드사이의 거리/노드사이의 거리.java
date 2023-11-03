2e32dd519a2c63d28f76f3b5ad83a0494845e07d
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-11-03 14:06:36 +0900

    [Gold V] Title: 노드사이의 거리, Time: 332 ms, Memory: 44508 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
@@ -3,55 +3,65 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int n,m;
+    static int n, m;
     static List<List<int[]>> list = new ArrayList<>();
-    public static void main(String[] args) throws Exception{
+    static int[] distance;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        for(int i=0; i<=n; i++){
-            list.add(new ArrayList<>());
-        }
-        for(int i=0; i<n-1; i++){
+
+        for (int i = 0; i <= n; i++) list.add(new ArrayList<>());
+
+        for (int i = 0; i < n - 1; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            list.get(a).add(new int[]{b,c});
-            list.get(b).add(new int[]{a,c});
+            list.get(a).add(new int[]{b, c});
+            list.get(b).add(new int[]{a, c});
         }
-        for(int i=0; i<m; i++){
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int temp = BFS(a,b);
-            sb.append(temp).append("\n");
+            visited = new boolean[n + 1];
+            distance = new int[n + 1];
+            BFS(a, b);
+            sb.append(distance[b]).append("\n");
         }
+
         System.out.println(sb);
+
+
     }
-    static int BFS(int a, int b){
+
+    static void BFS(int a, int b) {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(a);
-        int[] distance = new int[n+1];
-        Arrays.fill(distance, -1);
+        visited[a] = true;
         distance[a] = 0;
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int current = queue.poll();
-            for(int i=0; i<list.get(current).size(); i++){
+            if (current == b) return;
+            for (int i = 0; i < list.get(current).size(); i++) {
                 int[] next = list.get(current).get(i);
-                if(distance[next[0]] == -1){
-                    distance[next[0]] = distance[current] + next[1];
-                    queue.add(next[0]);
-                    if(next[0] == b)
-                        return distance[b];
-                }
+                int nextNode = next[0];
+                int nextCost = next[1];
+                if (visited[nextNode]) continue;
+                visited[nextNode] = true;
+                distance[nextNode] = distance[current] + nextCost;
+                queue.add(nextNode);
             }
-
         }
-        return -1;
     }
 
 }
+
 
