d256baccbd194ca5f5cbff94dc1e31da0f5af36f
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-11-03 15:16:49 +0900

    [Gold V] Title: 보물섬, Time: 516 ms, Memory: 294920 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.java
+++ 백준/Gold/2589. 보물섬/보물섬.java
@@ -1,61 +1,65 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n,m;
-    static char[][] arr;
-    static int[][] visited;
-    static int[] dx = {0,0,-1,1};
-    static int[] dy = {1,-1,0,0};
-    static int distance = Integer.MIN_VALUE;
-    public static void main(String[] args) throws Exception{
+    static int n, m;
+    static char[][] map;
+    static boolean[][] visited;
+    static int distance = 0;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        arr = new char[n][m];
-        visited = new int[n][m];
-        for(int i=0; i<n; i++){
+        visited = new boolean[n][m];
+        map = new char[n][m];
+
+        for (int i = 0; i < n; i++) {
             String s = br.readLine();
-            for(int j=0; j<m; j++){
-                arr[i][j] = s.charAt(j);
+            for (int j = 0; j < m; j++) {
+                map[i][j] = s.charAt(j);
             }
         }
 
-        for(int i=0; i<n; i++)
-            Arrays.fill(visited[i],-1);
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(arr[i][j] == 'L')
-                {
-                    BFS(i,j);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 'L') {
+                    visited = new boolean[n][m];
+                    BFS(i, j);
                 }
             }
         }
+
         System.out.println(distance);
     }
-    static void BFS(int a, int b){
-        for(int i=0; i<n; i++)
-            Arrays.fill(visited[i],-1);
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{a,b});
-        visited[a][b] = 0;
-        int count = 0;
-        while (!queue.isEmpty()){
-            int[] current = queue.poll();
-            for(int i=0; i<4; i++){
-                int nx = current[0] + dx[i];
-                int ny = current[1] + dy[i];
 
-                if(nx >=0 && nx<n && ny>=0 && ny<m && visited[nx][ny] == -1 && arr[nx][ny] == 'L'){
-                    visited[nx][ny] = visited[current[0]][current[1]] + 1;
-                    queue.add(new int[]{nx,ny});
-                    distance = Math.max(distance, visited[nx][ny]);
+    static void BFS(int a, int b) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{a, b, 0});
+        visited[a][b] = true;
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
+            int count = current[2];
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && map[nx][ny] == 'L' && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    queue.add(new int[]{nx, ny, count + 1});
+                    distance = Math.max(distance, count + 1);
                 }
             }
         }
     }
 
 }
+
+
