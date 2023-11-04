7ab34135562a17af00d0ee1a01a624650f9ba7b0
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-11-04 15:15:15 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 132 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<String, String> map = new HashMap<>();
+        map.put("NLCS", "North London Collegiate School");
+        map.put("BHA", "Branksome Hall Asia");
+        map.put("KIS", "Korea International School");
+        map.put("SJA", "St. Johnsbury Academy");
+
+        String s = br.readLine();
+        System.out.println(map.get(s));
+    }
+
+
+}
