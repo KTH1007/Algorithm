28db3eb596f6bb5bb1b265d799841a899666897e
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-11-06 13:14:36 +0900

    [Gold V] Title: 이진 검색 트리, Time: 632 ms, Memory: 19752 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -1,20 +1,34 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Node root = new Node(Integer.parseInt(br.readLine()));
+        String s;
+        while (true) {
+            s = br.readLine();
+            if (s == null || s.equals("")) break;
+            root.insert(Integer.parseInt(s));
+        }
+        postOrder(root);
+    }
+
+    static void postOrder(Node node) {
+        if (node == null) return;
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.num);
+
+    }
+
     static class Node {
         int num;
         Node left, right;
 
         Node(int num) {
             this.num = num;
-        }
-
-        Node(int num, Node left, Node right) {
-            this.num = num;
-            this.left = left;
-            this.right = right;
         }
 
         void insert(int n) {
@@ -27,26 +41,5 @@ public class Main {
             }
         }
     }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Node root = new Node(Integer.parseInt(br.readLine()));
-        String s;
-        while (true) {
-            s = br.readLine();
-            if (s == null || s.equals("")) break;
-            root.insert(Integer.parseInt(s));
-        }
-        postOrder(root);
-
-    }
-
-    static void postOrder(Node node) {
-        if (node == null) return;
-        postOrder(node.left);
-        postOrder(node.right);
-        System.out.println(node.num);
-    }
-
+    
 }
-
