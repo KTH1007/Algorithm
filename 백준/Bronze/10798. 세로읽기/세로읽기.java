4403392307e0bb1492a5d4e474515da68227a558
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-11-07 10:00:23 +0900

    [Bronze I] Title: 세로읽기, Time: 136 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[][] arr = new char[15][15];
+
+        for (int i = 0; i < 15; i++) {
+            String s = br.readLine();
+            if (s == null || s.equals("")) break;
+            for (int j = 0; j < s.length(); j++) {
+                arr[i][j] = s.charAt(j);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 15; j++) {
+                if (arr[j][i] == 0) continue;
+                sb.append(arr[j][i]);
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+
+}
