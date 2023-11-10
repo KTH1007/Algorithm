80d3d305adcddcde81a3d5f20a32756f9158ba90
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-11-10 17:55:31 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 124 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -5,30 +5,28 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int n, m;
     static int[] arr = new int[101];
     static int[] count = new int[101];
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < n; i++) {
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+
+        for (int i = 0; i < n + m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             arr[a] = b;
         }
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            arr[a] = b;
-        }
-        BFS();
+        bfs();
+
     }
 
-    static void BFS() {
+    static void bfs() {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(1);
         while (!queue.isEmpty()) {
@@ -39,15 +37,13 @@ public class Main {
             }
             for (int i = 1; i <= 6; i++) {
                 if (current + i > 100) continue;
-                //방문하지 않았고 사디라나 뱀이 없는 경우
+                //방문하지 않고 뱀이나 사다리가 없는 경우
                 if (count[current + i] == 0 && arr[current + i] == 0) {
                     count[current + i] = count[current] + 1;
                     queue.add(current + i);
-                }
-                //사다리나 뱀이 있는 경우
-                else if (arr[current + i] != 0) {
+                } else if (arr[current + i] != 0) { //사다리나 뱀이 있는 경우
                     int move = arr[current + i];
-                    //방문하지 않았던 곳
+                    //방문하지 않은 곳
                     if (count[move] == 0) {
                         count[move] = count[current] + 1;
                         queue.add(move);
