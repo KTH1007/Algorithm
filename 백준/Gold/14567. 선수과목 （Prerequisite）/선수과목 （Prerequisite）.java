be0f29530e1f45d5bf695def4d1cfaedcdaed8d7
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-11-10 11:11:12 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 700 ms, Memory: 128920 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
@@ -4,48 +4,45 @@ import java.util.*;
 
 public class Main {
     static int n, m;
-    static int[] parentNum;
-    static int[] answer;
     static List<List<Integer>> list = new ArrayList<>();
+    static int[] arr;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        parentNum = new int[n + 1];
-        answer = new int[n+1];
-        for (int i = 0; i <= n; i++) {
-            list.add(new ArrayList<>());
-        }
+        for (int i = 0; i <= n; i++) list.add(new ArrayList<>());
+        arr = new int[n + 1];
+
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             list.get(a).add(b);
-            parentNum[b]++;
+            arr[b]++;
         }
-        BFS();
-        for (int i = 1; i <= n; i++)
-            System.out.print(answer[i] + " ");
+
+        topologicalSort();
+        for (int i = 1; i < arr.length; i++) System.out.print(arr[i] + " ");
     }
 
-    static void BFS() {
+    static void topologicalSort() {
         Queue<Integer> queue = new LinkedList<>();
-        for(int i=1; i<=n; i++){
-            if(parentNum[i] == 0){
+        for (int i = 1; i <= n; i++) {
+            if (arr[i] == 0) {
                 queue.add(i);
-                answer[i] = 1;
+                arr[i] = 1;
             }
         }
         while (!queue.isEmpty()) {
             int current = queue.poll();
             for (int i = 0; i < list.get(current).size(); i++) {
                 int next = list.get(current).get(i);
-                parentNum[next]--;
-                if(parentNum[next] == 0){
+                arr[next]--;
+                if (arr[next] == 0) {
+                    arr[next] = arr[current] + 1;
                     queue.add(next);
-                    answer[next] = answer[current] + 1;
                 }
             }
         }
