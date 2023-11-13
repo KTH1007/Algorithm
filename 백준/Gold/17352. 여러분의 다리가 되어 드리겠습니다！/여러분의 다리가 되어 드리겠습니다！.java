790a7b3a4f64210b5055f0fb987f4d50ed7c388d
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-11-13 12:02:34 +0900

    [Gold V] Title: 여러분의 다리가 되어 드리겠습니다!, Time: 524 ms, Memory: 80348 KB -BaekjoonHub

--- 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.java
+++ 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.java
@@ -1,43 +1,51 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     static int n;
-    static List<List<Integer>> list = new ArrayList<>();
-    static boolean[] visited;
+    static int[] parent;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        for(int i=0; i<=n; i++){
-            list.add(new ArrayList<>());
-        }
-        visited = new boolean[n+1];
-        for(int i=0; i<n-2; i++){
-            st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        parent = new int[n + 1];
+        init();
+        for (int i = 0; i < n - 2; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            list.get(a).add(b);
-            list.get(b).add(a);
+            union(a, b);
         }
-        DFS(1);
-        for(int i=1; i<=n; i++){
-            if(!visited[i]){
-                System.out.println("1 " + i);
-                break;
+
+        for (int i = 2; i <= n; i++) {
+            if (!isSameParent(1, i)) {
+                System.out.println(1 + " " + i);
+                return;
             }
         }
     }
-    static void DFS(int v){
-        if(visited[v])
-            return;
-        visited[v] = true;
-        for(int i=0; i<list.get(v).size(); i++){
-            int next = list.get(v).get(i);
-            if(!visited[next])
-                DFS(next);
+
+    static void init() {
+        for (int i = 0; i <= n; i++) parent[i] = i;
+    }
+
+    static int find(int x) {
+        if (parent[x] == x) return x;
+        return find(parent[x]);
+    }
+
+    static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x != y) {
+            if (x < y) parent[y] = x;
+            else parent[x] = y;
         }
+    }
+
+    static boolean isSameParent(int x, int y) {
+        return find(x) == find(y);
     }
 
 
