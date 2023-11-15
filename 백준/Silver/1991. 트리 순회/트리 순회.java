f725eafb4203bda3394a488c51262c92bbe5bc26
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-11-15 15:29:52 +0900

    [Silver I] Title: 트리 순회, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -3,73 +3,71 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static Node head = new Node('A', null, null);
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st;
+        Node node = new Node('A', null, null);
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             char root = st.nextToken().charAt(0);
-            char left = st.nextToken().charAt(0);
-            char right = st.nextToken().charAt(0);
-
-            insertNode(head, root, left, right);
+            char leftNode = st.nextToken().charAt(0);
+            char rightNode = st.nextToken().charAt(0);
+            insertNode(node, root, leftNode, rightNode);
         }
-        preOrder(head);
+        preOrder(node);
         System.out.println();
-        inOrder(head);
+        inOrder(node);
         System.out.println();
-        postOrder(head);
+        postOder(node);
+
     }
 
-    static void insertNode(Node temp, char root, char left, char right) {
-        if (temp.value == root) {
-            if (left == '.') temp.left = null;
-            else temp.left = new Node(left, null, null);
-
-            if (right == '.') temp.right = null;
-            else temp.right = new Node(right, null, null);
+    static void insertNode(Node temp, char root, char leftNode, char rightNode) {
+        if (temp.node == root) {
+            if (leftNode == '.') temp.leftNode = null;
+            else temp.leftNode = new Node(leftNode, null, null);
+            if (rightNode == '.') temp.rigthNode = null;
+            else temp.rigthNode = new Node(rightNode, null, null);
         } else {
-            if (temp.left != null) insertNode(temp.left, root, left, right);
-            if (temp.right != null) insertNode(temp.right, root, left, right);
+            if (temp.leftNode != null) insertNode(temp.leftNode, root, leftNode, rightNode);
+            if (temp.rigthNode != null) insertNode(temp.rigthNode, root, leftNode, rightNode);
         }
-
     }
 
     static void preOrder(Node node) {
         if (node == null) return;
-        System.out.print(node.value);
-        preOrder(node.left);
-        preOrder(node.right);
+        System.out.print(node.node);
+        preOrder(node.leftNode);
+        preOrder(node.rigthNode);
     }
 
     static void inOrder(Node node) {
         if (node == null) return;
-        inOrder(node.left);
-        System.out.print(node.value);
-        inOrder(node.right);
+        inOrder(node.leftNode);
+        System.out.print(node.node);
+        inOrder(node.rigthNode);
     }
 
-    static void postOrder(Node node) {
+    static void postOder(Node node) {
         if (node == null) return;
-        postOrder(node.left);
-        postOrder(node.right);
-        System.out.print(node.value);
+        postOder(node.leftNode);
+        postOder(node.rigthNode);
+        System.out.print(node.node);
     }
 
     static class Node {
-        char value;
-        Node left;
-        Node right;
+        char node;
+        Node leftNode;
+        Node rigthNode;
 
-        Node(char value, Node left, Node right) {
-            this.value = value;
-            this.left = left;
-            this.right = right;
+        Node(char node, Node leftNode, Node rigthNode) {
+            this.node = node;
+            this.leftNode = leftNode;
+            this.rigthNode = rigthNode;
         }
     }
-
 
 }
