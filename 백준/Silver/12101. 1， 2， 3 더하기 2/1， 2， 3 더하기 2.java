20aa656582e90ed8e2424cc89d48496496de6712
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-11-20 16:56:20 +0900

    [Silver I] Title: 1, 2, 3 더하기 2, Time: 144 ms, Memory: 15828 KB -BaekjoonHub

--- 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
+++ 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
@@ -1,50 +1,43 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[] visited;
-    static boolean flag = false;
-    static int n;
-    static int k;
-    static int count = 0;
+    static int n, k;
     static int[] dp;
+    static int count = 0;
+    static boolean flag = false;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
+
         dp = new int[12];
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
-        visited = new boolean[n];
-        DFS(0, 0);
-        if(!flag)
-            System.out.println(-1);
+        dfs(0, 0);
+
+        if (!flag) System.out.println(-1);
+
     }
-    static void DFS(int depth, int num){
-        if(num > n){
-            return;
-        }
-        if(num == n){
+
+    static void dfs(int depth, int num) {
+        if (num > n) return;
+        if (num == n) {
             count++;
-            if(count == k){
+            if (count == k) {
                 flag = true;
-                for(int i=0; i<depth-1; i++){
-                    System.out.print(dp[i]+"+");
+                for (int i = 0; i < depth - 1; i++) {
+                    System.out.print(dp[i] + "+");
                 }
-                System.out.print(dp[depth-1]);
+                System.out.println(dp[depth - 1]);
             }
             return;
         }
-        for(int i=1; i<4; i++){
+        for (int i = 1; i < 4; i++) {
             dp[depth] = i;
-            DFS(depth+1, num+i);
+            dfs(depth + 1, num + i);
         }
     }
-
 
 }
