daed3b661465f2e8b040388160778e167fa36e05
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-11-22 11:17:49 +0900

    [Silver V] Title: 최대 상승, Time: 364 ms, Memory: 34224 KB -BaekjoonHub

--- 백준/Silver/25644. 최대 상승/최대 상승.java
+++ 백준/Silver/25644. 최대 상승/최대 상승.java
@@ -4,21 +4,21 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        // 현재값 - 최솟값
-        int answer = 0;
+
         int min = Integer.MAX_VALUE;
+        int max = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         while (n-- > 0) {
             int current = Integer.parseInt(st.nextToken());
             min = Math.min(min, current);
-            answer = Math.max(answer, current - min);
+            max = Math.max(max, current - min);
         }
-        System.out.println(answer);
 
+        System.out.println(max);
     }
 
 
