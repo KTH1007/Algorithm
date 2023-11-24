a470021178588c1a5b93b662c70a26cf69fee7f6
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-11-24 15:29:03 +0900

    [Bronze III] Title: 대지, Time: 360 ms, Memory: 27972 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if (n == 1) {
+            System.out.println(0);
+            return;
+        }
+        StringTokenizer st;
+        int[] x = new int[n];
+        int[] y = new int[n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            x[i] = Integer.parseInt(st.nextToken());
+            y[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(x);
+        Arrays.sort(y);
+
+        int result = ((x[x.length - 1] - x[0]) * (y[y.length - 1] - y[0]));
+        System.out.println(result);
+
+    }
+
+}
+
