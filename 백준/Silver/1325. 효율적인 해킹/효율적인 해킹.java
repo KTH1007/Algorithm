dbcf7707b1d58f9d5fd69529b407bfd465112c01
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-11-28 11:08:32 +0900

    [Silver I] Title: 효율적인 해킹, Time: 9832 ms, Memory: 305888 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -4,11 +4,10 @@ import java.util.*;
 
 public class Main {
     static int n, m;
-    static boolean[] visited;
     static List<List<Integer>> list = new ArrayList<>();
-    static int max = Integer.MIN_VALUE;
-    static int count;
-    static int[] arr;
+    static boolean[] visited;
+    static int[] hacking;
+    static Queue<Integer> queue = new LinkedList<>();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,9 +15,9 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        for (int i = 0; i <= n; i++) {
-            list.add(new ArrayList<>());
-        }
+
+        for (int i = 0; i <= n; i++) list.add(new ArrayList<>());
+        hacking = new int[n + 1];
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
@@ -27,31 +26,39 @@ public class Main {
             list.get(a).add(b);
         }
 
-        arr = new int[n + 1];
+        int max = 0;
+
         for (int i = 1; i <= n; i++) {
             visited = new boolean[n + 1];
-            BFS(i);
+            bfs(i);
         }
-        for (int i = 1; i <= n; i++)
-            max = Math.max(max, arr[i]);
+
+        for (int i : hacking) max = Math.max(max, i);
+
         for (int i = 1; i <= n; i++) {
-            if (arr[i] == max) sb.append(i + " ");
+            if (hacking[i] == max) sb.append(i + " ");
         }
+
         System.out.println(sb);
     }
 
-    static void BFS(int v) {
-        Queue<Integer> queue = new LinkedList<>();
+    static void bfs(int v) {
+        queue.clear();
         queue.add(v);
         visited[v] = true;
         while (!queue.isEmpty()) {
             int current = queue.poll();
             for (int next : list.get(current)) {
-                if (visited[next]) continue;
-                visited[next] = true;
-                queue.add(next);
-                arr[next]++;
+                if (!visited[next]) {
+                    queue.add(next);
+                    visited[next] = true;
+                    hacking[next]++;
+                }
             }
         }
+
     }
+
+
 }
+
