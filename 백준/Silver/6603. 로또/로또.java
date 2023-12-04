4943f33feee84057a59440e3c26b993454ddc5b8
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-12-04 13:38:58 +0900

    [Silver II] Title: 로또, Time: 132 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -10,22 +10,25 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         while (true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             n = Integer.parseInt(st.nextToken());
+
             if (n == 0) break;
+
             arr = new int[n];
             visited = new boolean[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(st.nextToken());
-            }
+
+            for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
             dfs(0, 0);
             sb.append("\n");
         }
         System.out.println(sb);
+
     }
 
-    static void dfs(int depth, int start) {
+    static void dfs(int depth, int at) {
         if (depth == 6) {
             for (int i = 0; i < n; i++) {
                 if (visited[i]) sb.append(arr[i]).append(" ");
@@ -33,14 +36,16 @@ public class Main {
             sb.append("\n");
             return;
         }
-        for (int i = start; i < n; i++) {
-            visited[i] = true;
-            dfs(depth + 1, i + 1);
-            visited[i] = false;
+        for (int i = at; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(depth + 1, i + 1);
+                visited[i] = false;
+            }
         }
     }
+
+
 }
-
-
 
 
