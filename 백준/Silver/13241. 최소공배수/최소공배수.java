c1911e85d3ac6e7ca617a47bbd42d4ee7480a910
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-12-07 10:45:55 +0900

    [Silver V] Title: 최소공배수, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Integer.parseInt(st.nextToken());
+        long b = Integer.parseInt(st.nextToken());
+
+        System.out.println((a * b) / gcd(a, b));
+
+    }
+
+    static long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+}
+
+
