e2060f02b94d2d45ad62f5e5c2d79d924ce2162c
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-12-15 09:45:37 +0900

    [Silver II] Title: 베르트랑 공준, Time: 856 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) break;
+
+            int count = 0;
+
+            for (int i = n + 1; i <= n * 2; i++) {
+                if (isPrime(i)) count++;
+            }
+            sb.append(count).append("\n");
+        }
+        System.out.println(sb);
+
+    }
+
+    static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+
+}
+
+
