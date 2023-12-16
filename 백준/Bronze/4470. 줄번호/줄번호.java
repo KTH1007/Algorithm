3c70c81321a9aab9879c6071473690d5e4d43ebd
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-12-16 15:26:47 +0900

    [Bronze IV] Title: 줄번호, Time: 128 ms, Memory: 13992 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            sb.append(i).append(". ").append(br.readLine()).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+
+}
+
+
