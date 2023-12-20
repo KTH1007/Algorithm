1375eec719162913f0d85221a367f11f68977a8a
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-12-20 10:46:33 +0900

    [Gold V] Title: 감소하는 수, Time: 128 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.java
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -1,44 +1,44 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.StringTokenizer;
 
 public class Main {
     static int n;
     static List<Long> list = new ArrayList<>();
-    static int count = 0;
 
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        //최대 10자리  9876543210 총 1024가지 방법 아무것도 선택하지 않으면 1023
-        if(n<10){
+        n = Integer.parseInt(br.readLine());
+
+        // 각 자리 마다 선택하거나 선택하지 않거나 하는 경우 2^10 = 1024
+        // 아무것도 선택하지 않는 경우 1을 빼면 1023가지 경우가 존재
+        if (n <= 10) {
             System.out.println(n);
             return;
-        }
-        else if(n > 1022){
+        } else if (n >= 1023) {
             System.out.println(-1);
             return;
         }
-        for(int i=0; i<10; i++){
-            DFS(i, 1);
+
+        for (int i = 0; i < 10; i++) {
+            dfs(i, 0);
         }
+
         Collections.sort(list);
         System.out.println(list.get(n));
 
     }
-    static void DFS(long num, int depth){
-        if(depth > 10)
-            return;
+
+    static void dfs(long num, int depth) {
+        if (depth > 10) return;
         list.add(num);
-        for(int i=0; i< num%10; i++){
-            DFS((num*10)+i, depth+1);
+        for (int i = 0; i < num % 10; i++) {
+            dfs((num * 10) + i, depth + 1);
         }
     }
 
 }
+
+
