e57c34df372ae0a7f0b54d9062558f1fc37b9635
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-12-20 15:11:35 +0900

    [Silver II] Title: 양 한마리... 양 두마리..., Time: 244 ms, Memory: 27112 KB -BaekjoonHub

--- 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.java
+++ 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.java
@@ -7,22 +7,25 @@ import java.util.StringTokenizer;
 public class Main {
     static int h, w;
     static char[][] arr;
-    static int count;
+    static boolean[][] visited;
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {1, -1, 0, 0};
-    static boolean[][] visited;
+    static int count = 0;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
         while (t-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             h = Integer.parseInt(st.nextToken());
             w = Integer.parseInt(st.nextToken());
+
             arr = new char[h][w];
             visited = new boolean[h][w];
-            count = 0;
+
             for (int i = 0; i < h; i++) {
                 String s = br.readLine();
                 for (int j = 0; j < w; j++) {
@@ -30,15 +33,15 @@ public class Main {
                 }
             }
 
+            count = 0;
             for (int i = 0; i < h; i++) {
                 for (int j = 0; j < w; j++) {
-                    if (!visited[i][j] && arr[i][j] == '#') {
-                        count++;
-                        bfs(i, j);
-                    }
+                    if (arr[i][j] == '#' && !visited[i][j]) bfs(i, j);
                 }
             }
+
             sb.append(count).append("\n");
+
         }
 
         System.out.println(sb);
@@ -51,18 +54,20 @@ public class Main {
         visited[a][b] = true;
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
-            int x = current[0];
-            int y = current[1];
             for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+                int nx = current[0] + dx[i];
+                int ny = current[1] + dy[i];
+
                 if (nx >= 0 && nx < h && ny >= 0 && ny < w && arr[nx][ny] == '#' && !visited[nx][ny]) {
                     visited[nx][ny] = true;
                     queue.add(new int[]{nx, ny});
                 }
             }
         }
+        count++;
     }
 
 
 }
+
+
