3e9ab00076edf0f595496a3fd3c192c5052aab2d
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2023-12-26 13:57:44 +0900

    [Gold V] Title: 노드사이의 거리, Time: 300 ms, Memory: 42324 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
@@ -5,12 +5,13 @@ import java.util.*;
 public class Main {
     static int n, m;
     static List<List<int[]>> list = new ArrayList<>();
-    static int[] distance;
     static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
@@ -25,42 +26,43 @@ public class Main {
             list.get(b).add(new int[]{a, c});
         }
 
-        StringBuilder sb = new StringBuilder();
-
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             visited = new boolean[n + 1];
-            distance = new int[n + 1];
-            BFS(a, b);
-            sb.append(distance[b]).append("\n");
+            bfs(a, b);
         }
 
         System.out.println(sb);
 
-
     }
 
-    static void BFS(int a, int b) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(a);
+    static void bfs(int a, int b) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{a, 0});
         visited[a] = true;
-        distance[a] = 0;
         while (!queue.isEmpty()) {
-            int current = queue.poll();
-            if (current == b) return;
-            for (int i = 0; i < list.get(current).size(); i++) {
-                int[] next = list.get(current).get(i);
-                int nextNode = next[0];
-                int nextCost = next[1];
-                if (visited[nextNode]) continue;
-                visited[nextNode] = true;
-                distance[nextNode] = distance[current] + nextCost;
-                queue.add(nextNode);
+            int[] current = queue.poll();
+            int x = current[0];
+            int distance = current[1];
+            for (int i = 0; i < list.get(x).size(); i++) {
+                int[] next = list.get(x).get(i);
+                int nx = next[0];
+                int nextDistacne = next[1];
+                if (!visited[nx]) {
+                    if (nx == b) {
+                        sb.append(distance + nextDistacne).append("\n");
+                        return;
+                    } else {
+                        visited[nx] = true;
+                        queue.add(new int[]{nx, distance + nextDistacne});
+                    }
+                }
             }
         }
     }
+
 
 }
 
