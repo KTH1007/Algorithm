f74af260ca2605d9d0488f362470c017c0b23196
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-01-06 12:38:38 +0900

    [Bronze IV] Title: 과자, Time: 128 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int total = k * n;
+
+        if (total > m) System.out.println(total - m);
+        else System.out.println(0);
+    }
+
+
+}
+
+
