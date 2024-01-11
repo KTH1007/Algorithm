6c50e959e92195de2810d8626dc45b4361a6be2b
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-01-11 10:43:18 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 152 ms, Memory: 22284 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -1,31 +1,25 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static long[] dp;
-    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int t = Integer.parseInt(st.nextToken());
-        dp = new long[1000001];
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[1000001];
         dp[1] = 1;
         dp[2] = 2;
-        dp[3] = 4;  //3을 나타내는 방법 1+1+1,  1+2, 2+1, 3
-        for (int i = 4; i <= 1000000; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000009;
-        }
+        dp[3] = 4;
+
+        for (int i = 4; i < 1000001; i++) dp[i] = (dp[i - 3] + dp[i - 2] + dp[i - 1]) % 1000000009;
+
         while (t-- > 0) {
-            n = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
             sb.append(dp[n]).append("\n");
         }
+
         System.out.println(sb);
     }
-
 }
-
-
-
