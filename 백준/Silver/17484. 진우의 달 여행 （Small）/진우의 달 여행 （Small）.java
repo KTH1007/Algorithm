0ca8ec0a3c5527b3a99fbba698a68af9fbe01ffe
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-01-15 10:30:00 +0900

    [Silver III] Title: 진우의 달 여행 (Small), Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.java
+++ 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.java
@@ -1,45 +1,54 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] dy = {-1, 0, 1};
     static int n, m;
     static int[][] arr;
+    static boolean[][] visited;
     static int min = Integer.MAX_VALUE;
+    static int[] dy = {-1, 0, 1};
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+
         arr = new int[n][m];
+
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+
         for (int i = 0; i < m; i++) {
-            int ans = arr[0][i];
-            dfs(0, i, 3, ans);
+            dfs(0, i, arr[0][i], -1);
         }
         System.out.println(min);
 
     }
 
-    static void dfs(int depth, int y, int dir, int ans) {
-        if (depth == n-1) {
-            min = Math.min(min, ans);
+
+    static void dfs(int depth, int y, int sum, int direction) {
+        if (depth == n - 1) {
+            min = Math.min(min, sum);
             return;
         }
+
         for (int i = 0; i < 3; i++) {
+            int nx = depth + 1;
             int ny = y + dy[i];
-            if (dir != i && ny >= 0 && ny < m) {
-                dfs(depth + 1, ny, i, ans + arr[depth+1][ny]);
+
+            if (nx < n && ny >= 0 && ny < m && direction != i) {
+                dfs(depth + 1, ny, sum + arr[nx][ny], i);
             }
         }
     }
-
 
 }
