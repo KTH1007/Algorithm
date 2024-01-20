68d1ab64cbc935472e2b2320cfab43251e78f2d3
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-01-20 20:26:40 +0900

    [Gold V] Title: 트리, Time: 124 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -3,26 +3,30 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int n, delete, count = 0;
     static int[] node;
+    static int n;
+    static int count = 0;
     static boolean[] visited;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
         node = new int[n];
         visited = new boolean[n];
+
         int root = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             node[i] = Integer.parseInt(st.nextToken());
             if (node[i] == -1) root = i;
         }
-        delete = Integer.parseInt(br.readLine());
+
+        int delete = Integer.parseInt(br.readLine());
+
         deleteNode(delete);
         countLeaf(root);
         System.out.println(count);
+
     }
 
     static void deleteNode(int d) {
@@ -37,7 +41,7 @@ public class Main {
         visited[c] = true;
         if (node[c] != -2) {
             for (int i = 0; i < n; i++) {
-                if (node[i] == c && !visited[i]) {
+                if (node[i] == c && !visited[i]) { //현재 노드를 부모로 가지고 있는 노드가 있으면 탐색
                     countLeaf(i);
                     isLeaf = false;
                 }
