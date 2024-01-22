1594c0db921eb51b2f904fc8b71ab3350a20b20d
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-01-22 09:52:20 +0900

    [Gold V] Title: 센서, Time: 196 ms, Memory: 16552 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.java
+++ 백준/Gold/2212. 센서/센서.java
@@ -4,35 +4,37 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
     static int n, k;
+    static int[] diff;
     static int[] arr;
+    static boolean[] visited;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         k = Integer.parseInt(br.readLine());
+
         arr = new int[n];
+        diff = new int[n - 1];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+
         Arrays.sort(arr);
-        if (k >= n) {
-            System.out.println(0);
-            return;
-        }
-        int[] diff = new int[n - 1];
+
         for (int i = 0; i < n - 1; i++) {
-            diff[i] = arr[i + 1] - arr[i];
+            diff[i] = Math.abs(arr[i] - arr[i + 1]);
         }
+
         Arrays.sort(diff);
+
         int sum = 0;
+
         for (int i = 0; i < n - k; i++) {
             sum += diff[i];
         }
         System.out.println(sum);
     }
-
-
 }
