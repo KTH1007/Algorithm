01d7948614042d1f1f1df2ae702734579559bdd7
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-01-24 08:55:30 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 120 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(br.readLine());
+
+        int sum = a + b;
+
+        if (sum >= c * 2) System.out.println(sum - c * 2);
+        else System.out.println(sum);
+    }
+}
