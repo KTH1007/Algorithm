9e1df0f5f589a849cd4128d6dda3acbdf54125c2
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-01-23 11:08:52 +0900

    [Silver I] Title: 회의실 배정, Time: 508 ms, Memory: 41700 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,22 +1,15 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][2];
-
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(arr, new Comparator<int[]>() {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
                 if (o1[1] == o2[1]) return o1[0] - o2[0];
@@ -24,16 +17,34 @@ public class Main {
             }
         });
 
-        int count = 0;
-        int endTime = 0;
+        StringTokenizer st;
         for (int i = 0; i < n; i++) {
-            if (arr[i][0] >= endTime) {
-                endTime = arr[i][1];
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            pq.add(new int[]{a, b});
+        }
+
+        int count = 0;
+
+        int[] current = pq.poll();
+
+        int start = current[0];
+        int end = current[1];
+        count++;
+
+        while (!pq.isEmpty()) {
+            current = pq.poll();
+            int nextStart = current[0];
+            int nextEnd = current[1];
+
+            if (end <= nextStart) {
+                end = nextEnd;
                 count++;
             }
         }
+
         System.out.println(count);
+
     }
-
-
 }
