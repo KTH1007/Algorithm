34f85d8423a598739cf0cb3a734b92aa5f50dde6
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-01-24 09:30:36 +0900

    [Silver I] Title: 1, 2, 3 더하기 2, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
+++ 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
@@ -4,40 +4,41 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int n, k;
-    static int[] dp;
+    static int[] arr;
     static int count = 0;
+    static StringBuilder sb = new StringBuilder();
     static boolean flag = false;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
+        arr = new int[12];
 
-        dp = new int[12];
         dfs(0, 0);
-
-        if (!flag) System.out.println(-1);
-
+        if (flag) System.out.println(sb);
+        else System.out.println(-1);
     }
 
-    static void dfs(int depth, int num) {
-        if (num > n) return;
-        if (num == n) {
+    static void dfs(int depth, int sum) {
+        if (sum > n) return;
+        if (sum == n) {
             count++;
             if (count == k) {
                 flag = true;
-                for (int i = 0; i < depth - 1; i++) {
-                    System.out.print(dp[i] + "+");
+                sb.append(arr[0]);
+                for (int i = 1; i < depth; i++) {
+                    sb.append("+").append(arr[i]);
                 }
-                System.out.println(dp[depth - 1]);
             }
             return;
         }
+
         for (int i = 1; i < 4; i++) {
-            dp[depth] = i;
-            dfs(depth + 1, num + i);
+            arr[depth] = i;
+            dfs(depth + 1, sum + i);
         }
     }
-
 }
