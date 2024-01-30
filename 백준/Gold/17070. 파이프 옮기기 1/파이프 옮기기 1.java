8093bb6aead96c1f3a078a54a09929e243159069
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-01-30 14:02:55 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 236 ms, Memory: 19644 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -3,48 +3,53 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int n;
     static int[][] arr;
-    static boolean[][] visited;
-    static int n, ans;
+    static int count = 0;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+
         arr = new int[n][n];
-        visited = new boolean[n][n];
+
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        ans = 0;
-        DFS(1, 0, 0);
-        System.out.println(ans);
-
+        dfs(0, 1, 1);
+        System.out.println(count);
     }
 
-    static void DFS(int x, int y, int direction) {
+
+    static void dfs(int x, int y, int direction) {
         if (x == n - 1 && y == n - 1) {
-            ans++;
-            return;
+            count++;
         }
-        //direction 0 -> 가로,  1 -> 세로, 2 -> 대각선
-        if (direction == 0) {
-            if (x + 1 < n && arr[y][x + 1] == 0) {
-                DFS(x + 1, y, 0);
+
+        if (direction == 1) {  //가로
+            if (y + 1 < n && arr[x][y + 1] != 1) {
+                dfs(x, y + 1, 1);
             }
-        } else if (direction == 1) {
-            if (y + 1 < n && arr[y + 1][x] == 0) DFS(x, y + 1, 1);
-        } else {
-            if (x + 1 < n && arr[y][x + 1] == 0) DFS(x + 1, y, 0);
-            if (y + 1 < n && arr[y + 1][x] == 0) DFS(x, y + 1, 1);
+        } else if (direction == 2) {  //세로
+            if (x + 1 < n && arr[x + 1][y] != 1) {
+                dfs(x + 1, y, 2);
+            }
+        } else if (direction == 3) {  //대각선
+            if (y + 1 < n && arr[x][y + 1] != 1) {
+                dfs(x, y + 1, 1);
+            }
+            if (x + 1 < n && arr[x + 1][y] != 1) {
+                dfs(x + 1, y, 2);
+            }
         }
-        //대각선은 가로, 세로, 대각선일 떄 모두 포함되기 때문에 아래로 빼줌
-        if (x + 1 < n && y + 1 < n && arr[y + 1][x] == 0 && arr[y][x + 1] == 0 && arr[y + 1][x + 1] == 0) {
-            DFS(x + 1, y + 1, 2);
+        if (x + 1 < n && y + 1 < n && arr[x][y + 1] != 1 && arr[x + 1][y] != 1 && arr[x + 1][y + 1] != 1) {
+            dfs(x + 1, y + 1, 3);
         }
     }
+
 
 }
