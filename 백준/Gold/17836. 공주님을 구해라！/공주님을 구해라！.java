faed2055a102a49ac78bee442deb8fa44c413b91
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-01-31 14:59:29 +0900

    [Gold V] Title: 공주님을 구해라!, Time: 180 ms, Memory: 15996 KB -BaekjoonHub

--- 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.java
+++ 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.java
@@ -15,12 +15,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         t = Integer.parseInt(st.nextToken());
 
         arr = new int[n][m];
-        visited = new boolean[n][m][2];  //검을 가졌을 때 왔던 길을 돌아가는 방법이 빠를 수 있음
+        visited = new boolean[2][n][m];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -28,41 +29,46 @@ public class Main {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+
         bfs();
+
         if (min > t) System.out.println("Fail");
         else System.out.println(min);
-
     }
 
     static void bfs() {
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0, 0, 0, 0});  //x y time gram
+        queue.add(new int[]{0, 0, 0, 0});
         visited[0][0][0] = true;
+
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
             int x = current[0];
             int y = current[1];
-            int time = current[2];
-            int gram = current[3];
-            if (x == n - 1 && y == m - 1) {
-                min = Math.min(min, time);
-            }
+            int gram = current[2];
+            int time = current[3];
+
+            if (x == n - 1 && y == m - 1) min = Math.min(min, time);
+
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && gram == 0 && !visited[nx][ny][0] && arr[nx][ny] != 1) {
-                    visited[nx][ny][0] = true;
-                    if (arr[nx][ny] == 2) {
-                        visited[nx][ny][1] = true;
-                        queue.add(new int[]{nx, ny, time + 1, 1});
-                    } else queue.add(new int[]{nx, ny, time + 1, gram});
 
-                } else if (nx >= 0 && nx < n && ny >= 0 && ny < m && gram == 1 && !visited[nx][ny][1]) {
-                    visited[nx][ny][1] = true;
-                    queue.add(new int[]{nx, ny, time + 1, gram});
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+                    if (gram == 0) {
+                        if (!visited[0][nx][ny] && arr[nx][ny] != 1) {
+                            visited[0][nx][ny] = true;
+                            if (arr[nx][ny] == 2) queue.add(new int[]{nx, ny, 1, time + 1});
+                            else queue.add(new int[]{nx, ny, 0, time + 1});
+                        }
+                    } else if (gram == 1) {
+                        if (!visited[1][nx][ny]) {
+                            visited[1][nx][ny] = true;
+                            queue.add(new int[]{nx, ny, gram, time + 1});
+                        }
+                    }
                 }
             }
-
         }
     }
 }
