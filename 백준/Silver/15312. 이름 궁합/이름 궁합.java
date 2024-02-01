81c6b31acce94f17f4cdcb1e538069392c8ec14b
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-02-01 11:42:47 +0900

    [Silver V] Title: 이름 궁합, Time: 192 ms, Memory: 16968 KB -BaekjoonHub

--- 백준/Silver/15312. 이름 궁합/이름 궁합.java
+++ 백준/Silver/15312. 이름 궁합/이름 궁합.java
@@ -2,27 +2,30 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int[] count = {3, 2, 1, 2, 3, 3, 2, 3, 3, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 2, 1};
-
+    static int[] alpha = {3, 2, 1, 2, 3, 3, 2, 3, 3, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 2, 1};
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s1 = br.readLine();
         String s2 = br.readLine();
-        int[] arr = new int[s1.length() + s2.length()];
-        for (int i = 0; i < arr.length - 1; i+=2) {
-            arr[i] = count[s1.charAt(i / 2) - 'A'];
-            arr[i + 1] = count[s2.charAt(i / 2) - 'A'];
+
+        int[] dp = new int[s1.length() + s2.length()];
+
+        for (int i = 0; i < s1.length() + s2.length() - 1; i += 2) {
+            dp[i] = alpha[s1.charAt(i / 2) - 'A'];
+            dp[i + 1] = alpha[s2.charAt(i / 2) - 'A'];
         }
-        int size = arr.length;
+
+        int size = dp.length;
+
         while (size > 2) {
             for (int i = 0; i < size - 1; i++) {
-                arr[i] = (arr[i] + arr[i + 1]) % 10;
+                dp[i] = (dp[i] + dp[i + 1]) % 10;
             }
             size--;
         }
-        System.out.println(arr[0] + "" + arr[1]);
-    }
 
+        System.out.println(dp[0] + "" + dp[1]);
+    }
 
 }
