04e1c8b504c05e7389474b891949baa485289cac
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-02-18 17:24:09 +0900

    [Silver I] Title: 1로 만들기 2, Time: 144 ms, Memory: 22020 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -1,36 +1,41 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int[] dp = new int[n+1];
-        int[] before = new int[n+1];
-        dp[1] = 0;
-        for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1] + 1;
-            before[i] = i-1;
-            if(i % 3 == 0 && dp[i/3] + 1 < dp[i] ){
-                dp[i] = dp[i/3] + 1;
-                before[i] = i/3;
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+        int[] before = new int[n + 1];
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+            before[i] = i - 1;
+
+            if (i % 3 == 0 && dp[i] > dp[i / 3] + 1) {
+                dp[i] = dp[i / 3] + 1;
+                before[i] = i / 3;
             }
-            if(i % 2 == 0 && dp[i/2] + 1 <dp[i]){
-                dp[i] = dp[i/2] + 1;
-                before[i] = i/2;
+            if (i % 2 == 0 && dp[i] > dp[i / 2] + 1) {
+                dp[i] = dp[i / 2] + 1;
+                before[i] = i / 2;
             }
         }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(dp[n]).append("\n");
 
         int m = n;
-        String s = "";
-        while (m > 0){
-            s += m + " ";
+
+        sb.append(n).append(" ");
+        while (m > 1) {
+            sb.append(before[m]).append(" ");
             m = before[m];
         }
-        System.out.println(dp[n]);
-        System.out.println(s);
-    }
-}
 
+        System.out.println(sb);
+    }
+
+}
