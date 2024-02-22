3cd1936ecf184c6741982bcad014b75ea9007dd7
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-02-22 16:48:51 +0900

    [Gold V] Title: 숫자고르기, Time: 120 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -1,44 +1,52 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     static int n;
-    static boolean[] visited;
     static int[] arr;
-    static int last;
+    static boolean[] visited;
+    static int last = 0;
     static List<Integer> list = new ArrayList<>();
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        n = Integer.parseInt(st.nextToken());
-        arr = new int[n+1];
-        visited = new boolean[n+1];
-        for(int i=1; i<=n; i++){
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        for(int i=1; i<=n; i++){
-            visited[i] = true;
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 1; i <= n; i++) arr[i] = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
             last = i;
-            DFS(i);
+            visited[i] = true;
+            dfs(i);
             visited[i] = false;
         }
+
+        StringBuilder sb = new StringBuilder();
+
         Collections.sort(list);
-        System.out.println(list.size());
-        for(int i=0; i<list.size(); i++){
-            System.out.println(list.get(i));
-        }
+        sb.append(list.size()).append("\n");
+        for (int i = 0; i < list.size(); i++) sb.append(list.get(i)).append("\n");
+
+        System.out.println(sb);
+
 
     }
-    static void DFS(int i){
-        if(!visited[arr[i]]){
+
+    static void dfs(int i) {
+        if (!visited[arr[i]]) {
             visited[arr[i]] = true;
-            DFS(arr[i]);
+            dfs(arr[i]);
             visited[arr[i]] = false;
         }
-        if(arr[i] == last)
-            list.add(arr[i]);
+        if (last == arr[i]) {
+            list.add(last);
+        }
     }
 
 }
