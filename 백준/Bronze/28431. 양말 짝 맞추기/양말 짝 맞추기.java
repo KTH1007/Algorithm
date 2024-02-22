752436ac1ac76c4d29c57abe9f3975b0dfa60907
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-02-22 09:10:23 +0900

    [Bronze IV] Title: 양말 짝 맞추기, Time: 120 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/28431. 양말 짝 맞추기/양말 짝 맞추기.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[10];
+        for (int i = 0; i < 5; i++) {
+            arr[Integer.parseInt(br.readLine())]++;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (arr[i] % 2 == 1) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+
+}
