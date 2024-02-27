25f9f150b98e97c8b77d2dde7b154ff154d56c9a
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-02-27 09:30:07 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 124 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -1,18 +1,19 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
         int n = Integer.parseInt(br.readLine());
 
         int result = 2;
-        for (int i = 1; i <= n; i++) {
-            result = result + (result - 1);
+        for (int i = 0; i < n; i++) {
+            result += (result - 1);
         }
         System.out.println(result * result);
     }
-
-
 }
