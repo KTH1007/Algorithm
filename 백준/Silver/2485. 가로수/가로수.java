d7f26b4400faba0cdef1957f7d6b6f4bf9f4c419
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-02-27 14:54:32 +0900

    [Silver IV] Title: 가로수, Time: 248 ms, Memory: 22416 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.java
+++ 백준/Silver/2485. 가로수/가로수.java
@@ -1,35 +1,31 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        List<Integer> list = new ArrayList<>();
+        int[] arr = new int[n];
 
-        for (int i = 0; i < n; i++) list.add(Integer.parseInt(br.readLine()));
-
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(br.readLine());
 
         int diff = 0;
-        int GCD = 0;
-        for (int i = 0; i < list.size() - 1; i++) {
-            diff = list.get(i + 1) - list.get(i);
-            GCD = gcd(diff, GCD);
+        int gcd = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            diff = arr[i + 1] - arr[i];
+            gcd = GCD(gcd, diff);  // 가로수 간격의 최대 공약수를 저장
         }
 
-        // 심은 나무 수 - 기존 나무 수
-        System.out.println((list.get(n - 1) - list.get(0)) / GCD + 1 - list.size());
-
+        //(arr[n-1] - arr[0]) / gcd -> 간격의 수
+        //간격의 수에 1을 더해야 가로수 총 개수l
+        //원래 가로수 개수를 빼줘야 함
+        System.out.println((arr[n - 1] - arr[0]) / gcd + 1 - n);
     }
 
-    static int gcd(int a, int b) {
+    static int GCD(int a, int b) {
         if (b == 0) return a;
-        return gcd(b, a % b);
+        return GCD(b, a % b);
     }
-
 }
-
-
