7c3b8e28f5c03587e007661c6dd400da25dda964
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-02-27 10:05:45 +0900

    [Silver V] Title: 분수찾기, Time: 148 ms, Memory: 16024 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -5,26 +5,19 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        /** 대각선으로 묶었을 떄 분수 분모의 합 - 1이 대각선 안의 분수의 개수
-         *  t가 홀수일 때 위부터 시작
-         *  t가 짝수일 때 아래부터 시작
-         */
         int n = Integer.parseInt(br.readLine());
-        //대각선 칸의 개수
+
+        // 현재 대각선 블록 개수
         int crossCount = 1;
-        //해당 대각선 직전 대각선까지 칸의 누적 합
+        // 직전 대각선까지의 블럭 개수
         int previousCountSum = 0;
 
         while (true) {
             if (n <= previousCountSum + crossCount) {
-                if (crossCount % 2 == 1) {
-                    // 분자가 큰 수부터 시작
-                    // 분자 n번째 - 직전 대각선까지의 블럭 개수 - 1
-                    // 분모 n번째 - 직전 대각선까지의 블럭 개수
-                    System.out.println((crossCount - (n - previousCountSum - 1)) + "/" + (n - previousCountSum));
+                if (crossCount % 2 == 1) { //대각선 개수가 홀수면 왼쪽 아래에서 오른쪽 위로 올라옴 즉 분자가 큰 수부터
+                    System.out.println(crossCount - (n - previousCountSum - 1) + "/" + (n - previousCountSum));
                     break;
                 } else {
-                    // 짝수의 경우 홀수 출력의 반대로
                     System.out.println((n - previousCountSum) + "/" + (crossCount - (n - previousCountSum - 1)));
                     break;
                 }
@@ -33,6 +26,5 @@ public class Main {
                 crossCount++;
             }
         }
-
     }
 }
