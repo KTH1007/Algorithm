19999b9e6e22ce9886dea99118d2f1ea2f851cba
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-02-28 17:10:15 +0900

    [Silver III] Title: queuestack, Time: 608 ms, Memory: 58704 KB -BaekjoonHub

--- 백준/Silver/24511. queuestack/queuestack.java
+++ 백준/Silver/24511. queuestack/queuestack.java
@@ -8,34 +8,29 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
 
         int[] A = new int[n];
-        Deque<Integer> deque = new ArrayDeque<>();
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) A[i] = Integer.parseInt(st.nextToken());
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            int a = Integer.parseInt(st.nextToken());
-            if (A[i] == 0) deque.add(a);
+            int num = Integer.parseInt(st.nextToken());
+            if (A[i] == 0) deque.add(num);
         }
 
         int m = Integer.parseInt(br.readLine());
-
         st = new StringTokenizer(br.readLine());
-        while (m-- > 0) {
-            int a = Integer.parseInt(st.nextToken());
-            deque.addFirst(a);
+        for (int i = 0; i < m; i++) {
+            deque.addFirst(Integer.parseInt(st.nextToken()));
             sb.append(deque.pollLast()).append(" ");
         }
 
         System.out.println(sb);
     }
 
-
 }
-
-
