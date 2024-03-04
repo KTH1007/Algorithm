9d83a0ce239bbd6e6eb74d3ba40406d24055f967
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-03-04 10:27:42 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 232 ms, Memory: 15564 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -1,42 +1,35 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
-    static List<Integer> list;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
+        boolean[] num = new boolean[1000001]; // false = 소수
+        num[0] = num[1] = true;
 
-        // flase = 소수
-        boolean[] prime = new boolean[1000001];
-
-        prime[0] = prime[1] = true;
-
-        for (int i = 2; i * i <= 1000000; i++) {
-            if (!prime[i]) {  // i가 소수라면
-                for (int j = i * i; j <= 1000000; j += i) prime[j] = true;
+        for (int i = 2; i * i < 1000001; i++) {
+            if (!num[i]) {
+                for (int j = i * i; j < 1000001; j += i) {
+                    num[j] = true;
+                }
             }
         }
 
+        StringBuilder sb = new StringBuilder();
+        int count;
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
-            list = new ArrayList<>();
-            int count = 0;
+            count = 0;
 
             for (int i = 2; i <= n / 2; i++) {
-                if (!prime[i] && !prime[n - i]) count++;
+                if (!num[i] && !num[n - i]) count++;
             }
+
             sb.append(count).append("\n");
         }
+
         System.out.println(sb);
-
     }
-
-
 }
-
-
