afaf7ca2f1720d3f09af043ea7a5f967bd4b9a5d
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-03-14 15:07:13 +0900

    [level 2] Title: 튜플, Time: 0.70 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,31 +1,18 @@
 import java.util.*;
 class Solution {
     public int[] solution(String s) {
-
-        s = s.substring(1, s.length() - 2);
-        s = s.replace("{", "");
-        String[] arr = s.split("},");
-        
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = arr[i].replace(",", " ");
-        }
-        
-        Arrays.sort(arr, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return o1.length() - o2.length();
-            }
-        });
+        String[] arr = s.replace("{", " ").replace("}", " ").trim().split(" , ");
+         
+        Arrays.sort(arr, (o1, o2) -> {return o1.length() - o2.length();});
         
         List<String> list = new ArrayList<>();
+        
         for (int i = 0; i < arr.length; i++) {
-            String[] temp = arr[i].split(" ");
-            for (int j = 0; j < temp.length; j++) {
-                if (!list.contains(temp[j])) {
-                    list.add(temp[j]);
-                }
+            for (String temp : arr[i].split(",")) {
+                if (!list.contains(temp)) list.add(temp);
             }
         }
+
         int[] answer = new int[list.size()];
         
         for (int i = 0; i < list.size(); i++) {
