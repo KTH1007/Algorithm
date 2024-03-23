790e8b99a7b1dcc10aa975af85b67b7b0d21bb91
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-03-23 14:23:27 +0900

    [Bronze III] Title: 삼각수의 합, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/2721. 삼각수의 합/삼각수의 합.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        while (t-- > 0) {
+            int sum = 0;
+            int n = Integer.parseInt(br.readLine());
+            for (int i = 1; i <= n; i++) {
+                sum += i * (i + 1) * (i + 2) / 2;
+            }
+            sb.append(sum).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+}
