f89a6864c9871b851748bfe00d6a6d4177d5f8e6
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-03-26 10:07:01 +0900

    [level 2] Title: 전화번호 목록, Time: 308.94 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -6,11 +6,12 @@ class Solution {
         Arrays.sort(phone_book);
         
         for (int i = 0; i < phone_book.length - 1; i++) {
-            int size = phone_book[i].length();
-            if (phone_book[i + 1].length() > size) {
-                String temp = phone_book[i + 1].substring(0, size);
-                if (temp.equals(phone_book[i])) return false;
-            } 
+            // int size = phone_book[i].length();
+            // if (phone_book[i + 1].length() > size) {
+            //     String temp = phone_book[i + 1].substring(0, size);
+            //     if (temp.equals(phone_book[i])) return false;
+            // } 
+            if (phone_book[i + 1].startsWith(phone_book[i])) return false;
         }
         return answer;
     }
