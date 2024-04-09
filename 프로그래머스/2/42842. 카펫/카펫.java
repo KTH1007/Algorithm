2c4a8dba9f37810971b39b5f8c0e19f108c5b5e0
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-04-09 10:41:22 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,14 +1,16 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
+        
         int sum = brown + yellow;
         // sum 약수 구하기
-        // 단 세로 길이가 3이상이어야 노란색 카펫을 감쌀 수 있음
+        // 세로 길이는 노란색을 감쌀 수 있게 최소 3이상
+        
         for (int i = 3; i < sum; i++) {
             int width = sum / i;
             
             if (width >= i) {
-                if ((i - 2) * (width - 2) == yellow) {
+                if ((width - 2) * (i - 2) == yellow) {
                     answer[0] = width;
                     answer[1] = i;
                     break;
