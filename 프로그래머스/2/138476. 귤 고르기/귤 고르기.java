99a474e657a30c8902ca4ddd5b40ba844725b540
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-04-16 11:01:51 +0900

    [level 2] Title: 귤 고르기, Time: 80.06 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -4,6 +4,7 @@ class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
+        
         for (int i = 0; i < tangerine.length; i++) {
             map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
         }
@@ -15,14 +16,19 @@ class Solution {
                 return map.get(o2) - map.get(o1);
             }
         });
-    
         
         for (int i = 0; i < list.size(); i++) {
             if (k <= 0) break;
-            answer++;
             k -= map.get(list.get(i));
+            answer++;
         }
         
         return answer;
     }
 }
+
+/*
+hashmap에 저장 후 같은 귤 크기 카운트 증가
+
+
+*/
