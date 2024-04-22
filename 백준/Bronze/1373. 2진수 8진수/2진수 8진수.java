fb6a22d9af2e32dd53803a12e9a359e85bd05d6f
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-04-22 10:29:47 +0900

    [Bronze I] Title: 2진수 8진수, Time: 324 ms, Memory: 21604 KB -BaekjoonHub

--- 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -6,29 +6,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
-        int n = s.length() / 3;
-        int remainder = s.length() % 3;
-
-
-        if (remainder == 1) {
-            s = "00" + s;
-        } else if (remainder == 2) {
-            s = "0" + s;
-        }
-
         StringBuilder sb = new StringBuilder();
 
+        if (s.length() % 3 == 1) sb.append(s.charAt(0));
+        if (s.length() % 3 == 2) sb.append((s.charAt(0) - '0') * 2 + (s.charAt(1) - '0'));
 
-        for (int i = 0; i < s.length(); i += 3) {
-            String temp = s.substring(i, i + 3);
-            if (temp.equals("000")) sb.append("0");
-            else if (temp.equals("001")) sb.append("1");
-            else if (temp.equals("010")) sb.append("2");
-            else if (temp.equals("011")) sb.append("3");
-            else if (temp.equals("100")) sb.append("4");
-            else if (temp.equals("101")) sb.append("5");
-            else if (temp.equals("110")) sb.append("6");
-            else if (temp.equals("111")) sb.append("7");
+        for (int i = s.length() % 3; i < s.length(); i += 3) {
+            sb.append((s.charAt(i) - '0') * 4 + (s.charAt(i + 1) - '0') * 2 + (s.charAt(i + 2) - '0'));
         }
 
         System.out.println(sb);
