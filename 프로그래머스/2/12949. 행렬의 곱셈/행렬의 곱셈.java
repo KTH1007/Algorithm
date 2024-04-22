674c4b77746675404263a9738483e26320620040
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-04-22 16:31:40 +0900

    [level 2] Title: 행렬의 곱셈, Time: 1.41 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,30 +1,47 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
         int[][] answer = new int[arr1.length][arr2[0].length];
+        
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr2[0].length; j++) {
                 int sum = 0;
-                for (int k = 0; k < arr1[1].length; k++) {
+                for (int k = 0; k < arr1[0].length; k++) {
                     sum += arr1[i][k] * arr2[k][j];
                 }
                 answer[i][j] = sum;
             }
         }
-        
         return answer;
     }
 }
-// 0 0 * 0 0 + 0 1 * 1 0 + 0 2 * 2 0
+
 /*
-2 3 2
-4 2 4
-3 1 4
+A
+[1, 4]
+[3, 2]
+[4, 1]
 
-5 4 ;
-2 4 1
-3 1 1
+B
+[3, 3]
+[3, 3]
 
-10 6 6, 8 12 2, 6 3 2
-20 4 12, 16 8 4, 12 2 4
-15 6 9
+1*3 + 4*3  1*3 + 4*3
+3*3 + 2*3  3*3 + 2*3
+4*3 + 1*3  4*3 + 1*3
+
+A
+[2, 3, 2]
+[4, 2, 4]
+[3, 1, 4]
+
+B
+[5, 4, 3]
+[2, 4, 1]
+[3, 1, 1]
+
+2*5 + 3*2 + 2*3
+4*5 + 2*2 + 4*3
+3*5 + 1*2 + 4*3
+
+r[0][0] = (a[0][0] * b[0][0]) + (a[0][1] * b[1][0]) + (a[0][2] * b[2][0])
 */
