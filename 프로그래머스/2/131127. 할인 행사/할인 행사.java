54db7ec3ada3fed1929b5c3651737053547cc103
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-04-25 16:49:41 +0900

    [level 2] Title: 할인 행사, Time: 0.09 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        HashMap<String, Integer> map = new HashMap<>();
+    
+        for (int i = 0; i < want.length; i++) {
+            map.put(want[i], number[i]);
+        }
+        
+        for (int i = 0; i < discount.length - 10 + 1; i++) {
+            HashMap<String, Integer> discountMap = new HashMap<>();
+            for (int j = 0; j < 10; j++) {
+                discountMap.put(discount[i + j],
+                                discountMap.getOrDefault(discount[i + j], 0) + 1);
+                
+            }
+            boolean flag = true;
+            for (String key : map.keySet()) {
+                if (map.get(key) != discountMap.get(key)) {
+                    flag = false;
+                    break;
+                }
+            }
+            
+            if (flag) answer++;
+        }
+        
+        
+        return answer;
+    }
+}
