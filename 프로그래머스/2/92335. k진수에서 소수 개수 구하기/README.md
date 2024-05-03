26e88a6dcf926a34df153e8f7d2b4f334ca5e6a4
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-05-03 10:55:49 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.07 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] k진수에서 소수 개수 구하기 - 92335 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335#qna) 
 
 ### 성능 요약
 
-메모리: 73.6 MB, 시간: 0.55 ms
+메모리: 74.1 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 27일 10:16:04
+2024년 05월 03일 10:55:42
 
 ### 문제 설명
 
--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,24 +1,22 @@
-import java.util.*;
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
         String s = Integer.toString(n, k);
-        System.out.println(s);
-        
-        
-        String[] arr = s.replace("0", " ").split("\\s+");
-        
+        // System.out.println(s);
+        String[] arr = s.split("0");
         for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("")) continue;
             long temp = Long.parseLong(arr[i]);
+            // System.out.println(temp);
+            
             if (isPrime(temp)) answer++;
-            System.out.println(arr[i]);
         }
         return answer;
     }
-    boolean isPrime(long n) {
-        if (n < 2) return false;
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) return false;
+    boolean isPrime(long num) {
+        if (num < 2) return false;
+        for (long i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
         }
         return true;
     }
