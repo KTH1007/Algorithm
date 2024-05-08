68e51f299137e3e0a0d87f52e5700a0fc9d25a2a
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-05-08 09:59:49 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.39 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/README.md
+++ 프로그래머스/2/92341. 주차 요금 계산/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주차 요금 계산 - 92341 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341#) 
 
 ### 성능 요약
 
-메모리: 76 MB, 시간: 0.29 ms
+메모리: 67.4 MB, 시간: 0.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 02일 15:28:14
+2024년 05월 08일 09:59:42
 
 ### 문제 설명
 
--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,72 +1,48 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] fees, String[] records) {
         StringTokenizer st;
         HashMap<String, Integer> map = new HashMap<>();
         HashMap<String, Integer> result = new HashMap<>();
-        ArrayList<String> carList = new ArrayList<>();
+        List<String> carList = new ArrayList<>();
         
         for (int i = 0; i < records.length; i++) {
-            st =  new StringTokenizer(records[i]);
+            String[] arr = records[i].split(" ");
+            String s = arr[0];
+            int time = (Integer.parseInt(s.substring(0,2)) * 60) + Integer.parseInt(s.substring(3,5));
+            String num = arr[1];
             
-            String temp = st.nextToken();
-            int hour = Integer.parseInt(temp.substring(0, 2)) * 60;
-            int minute = Integer.parseInt(temp.substring(3, 5));
-            int time = hour + minute;
+            if (!carList.contains(num)) {
+                carList.add(num);
+            }
             
-            String num = st.nextToken();
+            String inOut = arr[2];
             
-            String io = st.nextToken();
-            
-            // result map에는 시간만 넣기
-            
-            if (io.equals("IN")) {
-                if (!carList.contains(num)) {
-                    carList.add(num);
-                }
+            if (inOut.equals("IN")) {
                 map.put(num, time);
-            } else if (io.equals("OUT")) {
-                int tempTime = time - map.get(num);
-                result.put(num, result.getOrDefault(num, 0) + tempTime);
+            } else if (inOut.equals("OUT")) {
+                result.put(num, result.getOrDefault(num, 0) + time - map.get(num));
                 map.remove(num);
             }
         }
         
-        // 출차 기록이 없는 차량
+        int endTime = 23 * 60 + 59;
         for (String key : map.keySet()) {
-            int tempTime = 23 * 60 + 59 - map.get(key);
-            result.put(key, result.getOrDefault(key, 0) + tempTime);
+            result.put(key, result.getOrDefault(key, 0) + endTime - map.get(key));
         }
         
-        int[] answer = new int[carList.size()];
         Collections.sort(carList);
         
+        int[] answer = new int[carList.size()];
+        
         for (int i = 0; i < carList.size(); i++) {
-            int time = result.get(carList.get(i));
-            int totalFee = fee(fees, time);
-            answer[i] = totalFee;
+            if (result.get(carList.get(i)) <= fees[0]) {
+                answer[i] = fees[1];
+            } else {
+                answer[i] = fees[1] + (int) Math.ceil(((double) (result.get(carList.get(i))) - fees[0]) / fees[2]) * fees[3];
+            }
         }
         return answer;
     }
-    
-    public int fee(int[] fees, int time) {
-        if (time <= fees[0]) {
-            return fees[1];
-        } else {
-            return (int) (Math.ceil((double)(time - fees[0]) / fees[2])) * fees[3] + fees[1];
-        }
-    }
 }
-
-/*
-records 원소를 " " 단위로 분리해서 배열에 담기
-차량 입/출차 시간을 분으로 변환 substring(0, 2) * 60 + (3, 5)
-입차의 경우 map에 차량번호, 시간
-
-요금 = 출차의 경우 현재 시간 - map.get(차량번호)
-if 요금 <= 기본시간   -> 기본요금
-else 요금 / 단위 시간 * 단위 요금
-출차하면 map.remove(차량번호)
-출차 기록이 없는지 확인 방법
-map.keySet()으로 반복
-*/
