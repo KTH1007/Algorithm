bb7896004edc3edba2126b8c0ca0d8185e899c58
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-05-17 09:52:53 +0900

    [level 2] Title: 구명보트, Time: 8.30 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -5,11 +5,12 @@ class Solution {
         int answer = 0;
         
         Arrays.sort(people);
-        int count = 0;
         
-        for (int i = people.length - 1; i >= count; i--) {
-            if (people[i] + people[count] <= limit) {
-                count++;
+        int k = 0;
+        
+        for (int i = people.length - 1; i >= k; i--) {
+            if (people[i] + people[k] <= limit) {
+                k++;
             }
             answer++;
         }
