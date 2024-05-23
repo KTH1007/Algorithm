7d75fcbdcf1ea8ea55209dddb2cbaba8c9c6e453
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-05-23 13:38:43 +0900

    [Bronze I] Title: 반올림, Time: 124 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Bronze/2033. 반올림/반올림.java
+++ 백준/Bronze/2033. 반올림/반올림.java
@@ -5,32 +5,34 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String s = br.readLine();
+        int n = Integer.parseInt(br.readLine());
 
-        int temp = 0;
-        StringBuilder sb = new StringBuilder();
-        for (int i = s.length() - 1; i >= 0; i--) {
-            int n = s.charAt(i) - '0';
-            if (i == 0) {
-                if (temp != 0) {
-                    String string = sb.reverse().toString();
-                    sb.append(n + 1);
-                    System.out.println(n + 1 + string);
-                } else {
-                    sb.append(n);
-                    System.out.println(sb.reverse());
-                }
-                break;
-            }
-            if (temp == 10) {
-                n++;
-            }
-            if (n >= 5) temp = 10;
-            else temp = 0;
-            sb.append(temp % 10);
+        int result = 1;
+
+        if (n == 0) {
+            System.out.println(0);
+            return;
         }
 
-//        System.out.println(sb.reverse());
+        while (n > 0) {
+            if (n < 10) {
+                result *= n;
+                break;
+            }
+
+            int temp = n % 10;
+            int current = n / 10;
+
+            if (temp >= 5) {
+                current += 1;
+            }
+
+            n = current;
+            result *= 10;
+
+        }
+
+        System.out.println(result);
 
     }
 }
