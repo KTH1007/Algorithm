da6c5a9c9d568b4af79c00537e082cc51208a384
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-05-24 10:23:14 +0900

    [level 2] Title: H-Index, Time: 0.38 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -4,13 +4,10 @@ class Solution {
     public int solution(int[] citations) {
         int answer = 0;
         Arrays.sort(citations);
-        // 0 1 3 5 6
+        
         for (int i = 0; i < citations.length; i++) {
-            int h = citations.length - i; // 인용된 논문 수
-            if (citations[i] >= h) {
-                answer = h;
-                break;
-            }
+            int temp = citations.length - i;
+            answer = Math.max(answer, Math.min(temp, citations[i]));
         }
         return answer;
     }
