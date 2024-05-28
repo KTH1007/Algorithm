549c774885171a9c0f1d563f3b1863844541362b
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-05-28 11:21:35 +0900

    [level 2] Title: n^2 배열 자르기, Time: 5.35 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,16 +1,13 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int n, long left, long right) {
         int[] answer = new int[(int) (right - left) + 1];
         int index = 0;
         
-        // row col 중 max값 ex) (3, 1) -> 3 (1, 2) -> 2
-        
         for (long i = left; i <= right; i++) {
-            long row = i / n + 1;
-            long col = i % n + 1;
-            answer[index++] = (int) Math.max(row, col);
+            long row = i % n + 1;
+            long col = i / n + 1;
+            answer[index] = (int) Math.max(row, col);
+            index++;
         }
         return answer;
     }
