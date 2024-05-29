d098cfd0b02d2c06c372213c22b3c3cc09192363
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-05-29 11:31:50 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -5,15 +5,12 @@ class Solution {
         int answer = 1;
         HashMap<String, Integer> map = new HashMap<>();
         for (int i = 0; i < clothes.length; i++) {
-            // 옷의 개수 + 안 입은 경우
             map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 1) + 1);
         }
         
         for (String key : map.keySet()) {
             answer *= map.get(key);
         }
-        
-        // 아예 안 입은 경우 빼기
         return answer - 1;
     }
 }
