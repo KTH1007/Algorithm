ee46e47ef56b8ec9281acbbfcac52618d3a5b6c2
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-05-30 10:58:54 +0900

    [level 2] Title: 튜플, Time: 1.93 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/README.md
+++ 프로그래머스/2/64065. 튜플/README.md
@@ -1,10 +1,10 @@
 # [level 2] 튜플 - 64065 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065) 
 
 ### 성능 요약
 
-메모리: 81.7 MB, 시간: 0.61 ms
+메모리: 76.6 MB, 시간: 1.93 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 24일 14:08:08
+2024년 05월 30일 10:58:46
 
 ### 문제 설명
 
--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,28 +2,30 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-        s = s.substring(2, s.length() - 2);
-        String[] arr = s.replace("},{", " ").split(" ");
+        s = s.substring(2, s.length() - 2).replace("},{", " ");
+        String[] arr = s.split(" ");
         
-        Arrays.sort(arr, (o1, o2) -> o1.length() - o2.length());
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+        });
         
-        List<String> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         
         for (int i = 0; i < arr.length; i++) {
-            String[] temp = arr[i].split(",");
-            for (int j = 0; j < temp.length; j++) {
-                if (!list.contains(temp[j])) {
-                    list.add(temp[j]);
-                }
+            String[] newArr = arr[i].split(",");
+            for (int j = 0; j < newArr.length; j++) {
+                int n = Integer.parseInt(newArr[j]);
+                if (!list.contains(n)) list.add(n);
             }
         }
-        
         int[] answer = new int[list.size()];
         
         for (int i = 0; i < list.size(); i++) {
-            answer[i] = Integer.parseInt(list.get(i));
+            answer[i] = list.get(i);
         }
-        
         return answer;
     }
 }
