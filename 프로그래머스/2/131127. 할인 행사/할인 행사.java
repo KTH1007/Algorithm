214d2996ead8bbbc14a62f74e171e512a64f82d9
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-05-31 10:57:50 +0900

    [level 2] Title: 할인 행사, Time: 0.05 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,32 +1,36 @@
 import java.util.*;
+
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         HashMap<String, Integer> map = new HashMap<>();
-    
+        
         for (int i = 0; i < want.length; i++) {
             map.put(want[i], number[i]);
         }
         
-        for (int i = 0; i < discount.length - 10 + 1; i++) {
-            HashMap<String, Integer> discountMap = new HashMap<>();
-            for (int j = 0; j < 10; j++) {
-                discountMap.put(discount[i + j],
-                                discountMap.getOrDefault(discount[i + j], 0) + 1);
-                
+        
+        for (int i = 0; i <= discount.length - 10; i++) {
+            HashMap<String, Integer> temp = new HashMap<>();
+            for (int j = i; j < i + 10; j++) {
+                temp.put(discount[j], temp.getOrDefault(discount[j], 0) + 1);
             }
+            
             boolean flag = true;
-            for (String key : map.keySet()) {
-                if (map.get(key) != discountMap.get(key)) {
+            
+            for (int j = 0; j < want.length; j++) {
+                if (!temp.containsKey(want[j])) {
                     flag = false;
                     break;
                 }
+                if (map.get(want[j]) != temp.get(want[j])) {
+                    flag = false;
+                    break;
+                }
+                System.out.println("dd");
             }
-            
             if (flag) answer++;
         }
-        
-        
         return answer;
     }
 }
