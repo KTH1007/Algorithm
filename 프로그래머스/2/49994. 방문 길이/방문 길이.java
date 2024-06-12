a0c9a551162c62a832f2b1b9442a8d467cd1e7d2
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-06-12 09:42:27 +0900

    [level 2] Title: 방문 길이, Time: 0.30 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,26 +1,22 @@
-import java.util.*;
-
 class Solution {
+    
     public int solution(String dirs) {
         int answer = 0;
+        boolean[][][][] visited = new boolean[11][11][11][11];
         int x = 5;
         int y = 5;
-        
-        boolean[][][][] visited= new boolean[11][11][11][11];
-        
         int[] dx = {0, 0, -1, 1};
         int[] dy = {1, -1, 0, 0};
         int index = 0;
         
         for (int i = 0; i < dirs.length(); i++) {
             char c = dirs.charAt(i);
-            
             if (c == 'U') {
                 index = 0;
             } else if (c == 'D') {
                 index = 1;
-            } else if (c == 'L') {
-                index = 2;
+            } else if (c == 'L'){
+               index = 2;
             } else {
                 index = 3;
             }
@@ -29,14 +25,15 @@ class Solution {
             int ny = y + dy[index];
             
             if (nx >= 0 && nx < 11 && ny >= 0 && ny < 11) {
-                if (!visited[nx][ny][x][y] && !visited[x][y][nx][ny]) {
+                if (!visited[x][y][nx][ny] && !visited[nx][ny][x][y]) {
                     answer++;
-                    visited[nx][ny][x][y] = true;
                     visited[x][y][nx][ny] = true;
+                    visited[nx][ny][x][y] = true;
                 }
                 x = nx;
                 y = ny;
             }
+            
         }
         return answer;
     }
