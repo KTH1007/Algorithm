9fbd1cb787b571d24735fb31b249aed2eacd5888
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-06-14 10:33:04 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842#) 
 
 ### 성능 요약
 
-메모리: 78.7 MB, 시간: 0.03 ms
+메모리: 72.9 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 09일 10:41:16
+2024년 06월 14일 10:32:56
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -3,8 +3,6 @@ class Solution {
         int[] answer = new int[2];
         
         int sum = brown + yellow;
-        // sum 약수 구하기
-        // 세로 길이는 노란색을 감쌀 수 있게 최소 3이상
         
         for (int i = 3; i < sum; i++) {
             int width = sum / i;
@@ -14,7 +12,7 @@ class Solution {
                     answer[0] = width;
                     answer[1] = i;
                     break;
-                }
+                } 
             }
         }
         return answer;
