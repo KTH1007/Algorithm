e5dc82259ff32aea5aa2732e215b46f9e27fed67
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-06-17 10:17:24 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,16 +1,16 @@
 class Solution {
     public int solution(int[] arr) {
-        int answer = arr[0];
-        if (arr.length == 1) {
-            return arr[0];
-        }
+        int answer = 0;
+        if (arr.length == 1) return arr[0];
+        answer = arr[0] * arr[1] / gcd(arr[0], arr[1]);
+        
         for (int i = 1; i < arr.length; i++) {
             answer = answer * arr[i] / gcd(answer, arr[i]);
         }
         return answer;
     }
     
-    static int gcd(int a, int b) {
+    static int gcd(int a ,int b) {
         if (b == 0) return a;
         return gcd(b, a % b);
     }
