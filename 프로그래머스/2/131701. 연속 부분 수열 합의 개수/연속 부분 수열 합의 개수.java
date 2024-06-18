4d956e2c4eb39b14be0f603e4b7921dd5fd2a239
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-06-18 11:21:30 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1566.32 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -3,10 +3,16 @@ import java.util.*;
 class Solution {
     public int solution(int[] elements) {
         int answer = 0;
+        
         HashSet<Integer> set = new HashSet<>();
         
         int start = 1;
         
+        /*
+        start -> 더할 원소의 개수를 정함
+        i -> 시작할 인덱스
+        j -> j번째 원소를 더함
+        */
         while (start <= elements.length) {
             for (int i = 0; i < elements.length; i++) {
                 int sum = 0;
