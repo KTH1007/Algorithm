512eb3f4b52effd2f76978f04c3feba108d8cca9
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-06-18 13:20:44 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 241.22 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속 부분 수열 합의 개수 - 131701 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701) 
 
 ### 성능 요약
 
-메모리: 131 MB, 시간: 1566.32 ms
+메모리: 134 MB, 시간: 241.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 18일 11:21:22
+2024년 06월 18일 13:20:39
 
 ### 문제 설명
 
--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -6,22 +6,14 @@ class Solution {
         
         HashSet<Integer> set = new HashSet<>();
         
-        int start = 1;
+        int sum = 0;
         
-        /*
-        start -> 더할 원소의 개수를 정함
-        i -> 시작할 인덱스
-        j -> j번째 원소를 더함
-        */
-        while (start <= elements.length) {
+        for (int len = 1; len <= elements.length; len++) {
+            sum = 0;
             for (int i = 0; i < elements.length; i++) {
-                int sum = 0;
-                for (int j = i; j < start + i; j++) {
-                    sum += elements[j % elements.length];
-                }
+                sum += elements[(len + i - 1) % elements.length];
                 set.add(sum);
             }
-            start++;
         }
         answer = set.size();
         return answer;
