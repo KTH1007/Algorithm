7e9a2a77839dde63884037d850ca45f735c8a22c
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-06-24 10:08:05 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.09 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -3,23 +3,20 @@ import java.util.*;
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        
         String s = Integer.toString(n, k);
         
         String[] arr = s.split("0");
-        
-        for (String string : arr) {
-            if (!string.equals("")){
-                long temp = Long.parseLong(string);
-                System.out.println(temp);
-                if (isPrime(temp))
+        for (String a : arr) {
+            if (!a.equals("")) {
+                if (isPrime(Long.parseLong(a))) {
                     answer++;
+                }
             }
         }
-        
         return answer;
     }
-    public static boolean isPrime(long n) {
+    
+    static boolean isPrime(long n) {
         if (n < 2) return false;
         for (long i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) return false;
