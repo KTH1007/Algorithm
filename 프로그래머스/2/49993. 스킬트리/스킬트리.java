8a5fb6f645d691867054e47a896166c500b5c1ad
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-06-26 11:13:49 +0900

    [level 2] Title: 스킬트리, Time: 9.12 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,17 +1,15 @@
+import java.util.*;
+
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
+        for (int i = 0; i < skill_trees.length; i++) {
+            skill_trees[i] = skill_trees[i].replaceAll("[^" + skill + "]", "");
+        }
         
         for (int i = 0; i < skill_trees.length; i++) {
-            skill_trees[i] = skill_trees[i].replaceAll("[^" + skill +"]", "");
-            System.out.println(skill_trees[i]);
-            
-            for (int j = 0; j < skill.length() + 1; j++) {
-                if (skill_trees[i].equals(skill.substring(0, j))) {
-                    answer++;
-                    break;
-                }
-            }
+            int length = skill_trees[i].length();
+            if (skill.substring(0, length).equals(skill_trees[i])) answer++;
         }
         return answer;
     }
