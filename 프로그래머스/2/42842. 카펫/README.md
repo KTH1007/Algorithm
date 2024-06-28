bef23af5d7dc3fd9cc70a35ee9ba3478b82cf52e
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-06-28 13:50:33 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 72.9 MB, 시간: 0.01 ms
+메모리: 70.8 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 14일 10:32:56
+2024년 06월 28일 13:50:24
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,18 +1,16 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        
         int sum = brown + yellow;
-        
         for (int i = 3; i < sum; i++) {
-            int width = sum / i;
-            
-            if (width >= i) {
-                if ((width - 2) * (i - 2) == yellow) {
-                    answer[0] = width;
-                    answer[1] = i;
-                    break;
-                } 
+            if (sum % i == 0) {
+                int w = i;
+                int h = sum / i;
+                
+                if ((h - 2) * (w - 2) == yellow) {
+                    answer[0] = w;
+                    answer[1] = h;
+                }
             }
         }
         return answer;
