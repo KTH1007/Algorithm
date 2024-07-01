c8410eef62acab62d53d1cf14e073c1698def0c1
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-07-01 11:40:13 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 164.86 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -3,16 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(int[] elements) {
         int answer = 0;
-        
         HashSet<Integer> set = new HashSet<>();
-        
-        int sum = 0;
-        
-        for (int len = 1; len <= elements.length; len++) {
-            sum = 0;
-            for (int i = 0; i < elements.length; i++) {
-                sum += elements[(len + i - 1) % elements.length];
+        for (int i = 0; i < elements.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < elements.length; j++) {
+                sum += elements[(i + j) % elements.length];
                 set.add(sum);
+                
             }
         }
         answer = set.size();
