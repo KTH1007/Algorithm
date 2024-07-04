c3e5edd3a456a1693368950c72c13bcbcb3e1f3b
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-07-04 10:50:01 +0900

    [level 2] Title: 프로세스, Time: 0.50 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,17 +1,16 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 1;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for (int i = 0; i < priorities.length; i++) {
             pq.add(priorities[i]);
         }
         
         while (!pq.isEmpty()) {
             for (int i = 0; i < priorities.length; i++) {
-                if (priorities[i] == pq.peek()) {
+                if (pq.peek() == priorities[i]) {
                     if (i == location) {
                         return answer;
                     }
