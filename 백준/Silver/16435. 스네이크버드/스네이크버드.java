12a7d2d302250cb3d619b4b6d0466c8ed1b4d637
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-07-08 08:54:22 +0900

    [Silver V] Title: 스네이크버드, Time: 136 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int length = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            if (length >= arr[i]) {
+                length++;
+            }
+        }
+
+        System.out.println(length);
+        br.close();
+    }
+
+}
