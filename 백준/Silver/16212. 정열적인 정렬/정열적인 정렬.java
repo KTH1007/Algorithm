5ef717e3ef588532d4d0cab4b1555f3caeb52196
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-08-06 09:06:50 +0900

    [Silver V] Title: 정열적인 정렬, Time: 1200 ms, Memory: 117436 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        List<Integer> list = new ArrayList<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        list.stream().sorted().forEach(i -> sb.append(i).append(" "));
+        System.out.println(sb);
+        br.close();
+    }
+}
