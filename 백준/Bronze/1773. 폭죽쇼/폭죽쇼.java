49b19d027bd5a84a9ed0cfdbd93018f70c74bbb9
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-09-19 13:18:11 +0900

    [Bronze II] Title: 폭죽쇼, Time: 604 ms, Memory: 14652 KB -BaekjoonHub

---
+++ 백준/Bronze/1773. 폭죽쇼/폭죽쇼.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+//import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
+		int count = 0;
+		for(int i=0;i<N;i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			}
+		
+		for(int j=1;j<=C;j++) { 
+			for(int i=0;i<N;i++) {
+				int X =j%arr[i];
+				if(X==0) {
+					count = count+1;
+					break;
+				}	
+			}
+		}
+		bw.write(count +"\n");	
+		bw.close();
+		br.close();
+	}
+}
