4109acfc4e6f519ee72819f7645b270f1041cd62
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-10-11 13:15:39 +0900

    [Bronze V] Title: 꼬마 정민, Time: 108 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -1,19 +1,19 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
-
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        BigInteger a = new BigInteger(st.nextToken());
-        BigInteger b = new BigInteger(st.nextToken());
-        BigInteger c = new BigInteger(st.nextToken());
-        System.out.println(a.add(b).add(c));
+
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+        BigInteger C = new BigInteger(st.nextToken());
+
+        BigInteger sum = A.add(B).add(C);
+        System.out.println(sum);
     }
-
-
 }
