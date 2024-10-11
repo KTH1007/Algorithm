b5320bdf09929aeb148005dd21cb14455bdf54a1
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-10-11 11:14:41 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 100 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -1,16 +1,24 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
-        StringBuilder sb = new StringBuilder();
-        String s2 = sb.append(s).reverse().toString();
-        if (s.equals(s2)) System.out.println(1);
-        else System.out.println(0);
+
+        boolean flag = true;
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - i - 1)) {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
     }
-
-
 }
