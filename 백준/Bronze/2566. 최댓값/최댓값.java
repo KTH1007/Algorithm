3e3526fee85f5681c6db76380c273483ad0575bc
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-10-14 10:42:08 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 15976 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,30 +1,37 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int max = -1;
-        int a = 0;
-        int b = 0;
+        int[][] arr = new int[9][9];
+        int max = Integer.MIN_VALUE;
+        int x = 0;
+        int y = 0;
 
-        for (int i = 1; i < 10; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 1; j < 10; j++) {
-                int n = Integer.parseInt(st.nextToken());
-                if (n > max) {
-                    max = n;
-                    a = i;
-                    b = j;
+        StringTokenizer st;
+
+        for (int i = 0; i < 9; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                    x = i + 1;
+                    y = j + 1;
                 }
             }
         }
 
         System.out.println(max);
-        System.out.println(a + " " + b);
+        System.out.println(x + " " + y);
     }
-
-
 }
