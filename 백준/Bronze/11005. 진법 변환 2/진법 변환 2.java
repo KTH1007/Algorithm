4b5fa941d12e1315d0385d3df1b64a2f1c1f7dad
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-10-20 19:43:59 +0900

    [Bronze I] Title: 진법 변환 2, Time: 100 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -1,16 +1,17 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
+
+        int n = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        System.out.println(Integer.toString(a, b).toUpperCase());
+
+        System.out.println(Integer.toString(n, b).toUpperCase());
     }
-
-
 }
