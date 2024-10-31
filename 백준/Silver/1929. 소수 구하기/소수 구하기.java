c6f99477c8f3aac60c6358bb47ae242aa0234d4f
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-10-31 10:05:00 +0900

    [Silver III] Title: 소수 구하기, Time: 224 ms, Memory: 20676 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,34 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
-        int m = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
-        List<Integer> list = new ArrayList<>();
-        for(int i=m; i<=n; i++){
-            if(i<2)
-                continue;
-            if(isPrime(i))
-                list.add(i);
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        }
-        for(int i=0; i<list.size(); i++){
-            sb.append(list.get(i)).append("\n");
-        }
-        System.out.println(sb);
-    }
-    static boolean isPrime(int n){
-        for(int i = 2; i<=Math.sqrt(n); i++){
-            if(n % i == 0){
-                return false;
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        boolean[] isPrime = prime(N);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = M; i <= N; i++) {
+            if (isPrime[i]) {
+                sb.append(i).append("\n");
             }
         }
-        return true;
+
+        System.out.println(sb);
+    }
+
+    private static boolean[] prime(int n) {
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        return isPrime;
     }
 }
