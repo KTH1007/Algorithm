708b25d6f753b5a42bbc646672a2525a68af9bf2
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-11-04 16:08:53 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,25 +1,35 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
-        st = new StringTokenizer(br.readLine());
+        List<Integer> list = new ArrayList<>();
+
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            list.add(Integer.parseInt(st.nextToken()));
         }
+
         int v = Integer.parseInt(br.readLine());
+
+        Collections.sort(list);
+
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (v == arr[i]) count++;
+        if (list.contains(v)) {
+            count = list.lastIndexOf(v) - list.indexOf(v) + 1;
+        } else {
+            count = 0;
         }
+
         System.out.println(count);
     }
-
-
 }
