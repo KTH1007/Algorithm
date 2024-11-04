3f2aad2a8eccc18e99060d7ff68d252711b949cb
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-11-04 15:46:34 +0900

    [Bronze III] Title: 학점계산, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/2754. 학점계산/학점계산.java
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -1,10 +1,11 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         HashMap<String, String> map = new HashMap<>();
         map.put("A+", "4.3");
@@ -22,6 +23,4 @@ public class Main {
         map.put("F", "0.0");
         System.out.println(map.get(br.readLine()));
     }
-
-
 }
