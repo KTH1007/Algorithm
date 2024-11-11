51708f2d3ea89864159430d3a4d4ac02f41c6bdb
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-11-11 14:38:16 +0900

    [Bronze III] Title: 알람 시계, Time: 128 ms, Memory: 16108 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,31 +1,28 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String s = br.readLine();
-		StringTokenizer st = new StringTokenizer(s);
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		b -= 45;
-		if(b < 0) {
-			b = 60 + b;
-			a--;
-			if(a<0) {
-				a = 23;
-			}
-		}
-		bw.write(Integer.toString(a) + " ");
-		bw.write(Integer.toString(b));
-		bw.flush();
-		bw.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	}
+        int hour = Integer.parseInt(st.nextToken());
+        int second = Integer.parseInt(st.nextToken());
+
+        if (second - 45 >= 0) {
+            second -= 45;
+        } else {
+            hour--;
+            second = second + 15;
+        }
+
+        if (hour < 0) {
+            hour = 23;
+        }
+
+        System.out.println(hour + " " + second);
+    }
 }
