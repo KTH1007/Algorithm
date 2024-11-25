e790629705b1960e1adb42696814c263975f092a
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-11-25 11:44:34 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,31 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+
         int count = 0;
-        while (true){
-            if(n % 5 == 0){
-                count += n/5;
+        while (true) {
+            if (n % 5 == 0) {
+                count += n / 5;
                 break;
             }
-            else {
-                n -= 3;
-                count++;  // 5로 안 나눠지면 3킬로그램 봉지 하나 추가
-            }
-            if(n<0){
+            if (n < 0) {
                 count = -1;
                 break;
             }
+            n -= 3;
+            count++;
         }
-        System.out.println(count);
 
+        System.out.println(count);
 
     }
 }
