e8dd816130f332f6eee880162fc965ca1aa6caf6
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-12-01 23:09:35 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while (n > 0) {
+            list.add(n % 10);
+            n /= 10;
+        }
+
+        list.sort(Comparator.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+
+        for (Integer i : list) {
+            sb.append(i);
+        }
+
+        System.out.println(sb);
+    }
+}
