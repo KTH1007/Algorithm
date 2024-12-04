9fe715fec0149fd0fc72395bb9698a4f096b8eb9
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-12-04 11:04:22 +0900

    [Bronze V] Title: A+B - 7, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -1,23 +1,21 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
 
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            sb.append("Case #").append(i).append(": ").append(a + b).append("\n");
+            sb.append("Case #").append(i).append(": ").append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append("\n");
         }
+
         System.out.println(sb);
     }
-
-
 }
