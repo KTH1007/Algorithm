d93d0a3670256f0acb935ef3ad060b854e649658
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-12-09 13:56:59 +0900

    [Silver IV] Title: 듣보잡, Time: 252 ms, Memory: 27124 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,55 +1,42 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String ss = bf.readLine();
-		StringTokenizer st = new StringTokenizer(ss);
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		//중복일 경우 value에1 더하기 값이 1인 key만 출력
-		int cnt = 0;
-		HashMap<String, Integer> map = new HashMap<String, Integer>();
-		for (int i = 0; i < a; i++) {
-			map.put(bf.readLine(), 0);
-		}
-		for (int i = 0; i < b; i++) {
-			String s = bf.readLine();
-			if (map.containsKey(s)) {
-				cnt++;
-				map.replace(s, map.get(s)+1);
-			}
-			else {
-				map.put(s, 0);
-			}
-		}
-		
-		bw.write(Integer.toString(cnt));
-		bw.write("\n");
-		String[] arr = new String[cnt];
-		int j = 0;
-		for(String i : map.keySet()) {
-			if(map.get(i) == 1) {
-				arr[j] = i;
-				j++;
-			}
-		}
-		Arrays.sort(arr);
-		for(int i=0; i<arr.length; i++) {
-			bw.write(arr[i]);
-			bw.write("\n");
-		}
-		bw.flush();
-		bw.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	}
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        List<String> list = new ArrayList<>();
+
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            String s = br.readLine();
+            if (set.contains(s)) {
+                count++;
+                list.add(s);
+            }
+        }
+
+        list.sort(null);
+
+        for (String s : list) {
+            sb.append(s).append("\n");
+        }
+
+        System.out.println(count);
+        System.out.println(sb);
+    }
 }
