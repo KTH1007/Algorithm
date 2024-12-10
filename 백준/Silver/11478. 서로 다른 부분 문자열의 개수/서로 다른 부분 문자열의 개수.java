88b7de20c10259baad14f4037f8a56a48a851ede
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-12-11 00:17:26 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 672 ms, Memory: 226968 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
@@ -2,21 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
-import java.util.StringTokenizer;
+import java.util.Set;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		//int n = Integer.parseInt(st.nextToken());
-		String s = st.nextToken();
-		HashSet<String> set = new HashSet<String>();
-		for(int i=0; i<=s.length(); i++) {
-			for(int j=i+1; j<=s.length(); j++) {
-				set.add(s.substring(i,j));
-			}
-		}
-		System.out.println(set.size());
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                set.add(s.substring(i, j));
+            }
+        }
+
+        System.out.println(set.size());
+    }
 }
