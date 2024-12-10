8832bdcd3f8546331cffb894517e286a68ee4605
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-12-10 23:48:15 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 108 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,22 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n-i; j++){
-                System.out.print(" ");
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                sb.append(" ");
             }
-            for(int k=0; k<i; k++){
-                System.out.print("*");
+            for (int j = 0; j < i + 1; j++) {
+                sb.append("*");
             }
-            System.out.println();
+            sb.append("\n");
         }
-        scanner.close();
+
+        System.out.println(sb);
     }
 }
-        
-
-        
