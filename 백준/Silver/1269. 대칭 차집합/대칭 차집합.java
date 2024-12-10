0ef9f8cc0d87d90adf435fd55af506317db48dd9
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-12-11 00:06:12 +0900

    [Silver IV] Title: 대칭 차집합, Time: 792 ms, Memory: 82116 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -1,35 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashSet;
-import java.util.Scanner;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();			
-		HashSet<Integer> hs = new HashSet<Integer>();
-		HashSet<Integer> hs2 = new HashSet<Integer>();
-		
-		for(int i=0; i<n; i++) {
-			hs.add(sc.nextInt());
-		}
-		for(int i=0; i<m; i++) {
-			hs2.add(sc.nextInt());
-		}
-		int cnt = 0;
-		for(int i : hs) {
-			if(!hs2.contains(i)) {
-				cnt++;
-			}
-		}
-		
-		for(int i : hs2) {
-			if(!hs.contains(i)) {
-				cnt++;
-			}
-		}
-		
-		System.out.println(cnt);
-		sc.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Set<Integer> A = new HashSet<>();
+        Set<Integer> B = new HashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            A.add(Integer.parseInt(st.nextToken()));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            B.add(Integer.parseInt(st.nextToken()));
+        }
+
+
+        for (Integer i : B) {
+            if (A.contains(i)) n--;
+        }
+
+        for (Integer i : A) {
+            if (B.contains(i)) m--;
+        }
+
+        System.out.println(n + m);
+
+    }
 }
