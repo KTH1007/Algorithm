f4a1e53fa048a7fcc638e1e84b2471d37ff2acc4
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-12-17 20:58:19 +0900

    [Silver II] Title: 베르트랑 공준, Time: 152 ms, Memory: 14728 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,36 +1,41 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-
-    public static void main(String[] args) throws Exception {
+    static final int NUM = 246912;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
+        boolean[] prime = new boolean[NUM + 1];
+        Arrays.fill(prime, true);
+        prime[0] = prime[1] = false;
+        isPrime(prime);
+
+        StringBuilder sb = new StringBuilder();
         while (true) {
             int n = Integer.parseInt(br.readLine());
             if (n == 0) break;
 
             int count = 0;
-
-            for (int i = n + 1; i <= n * 2; i++) {
-                if (isPrime(i)) count++;
+            for (int i = n + 1; i <= 2 * n; i++) {
+                if (prime[i]) count++;
             }
+
             sb.append(count).append("\n");
         }
+
         System.out.println(sb);
-
     }
 
-    static boolean isPrime(int n) {
-        if (n < 2) return false;
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) return false;
+    private static void isPrime(boolean[] prime) {
+        for (int i = 2; i <= Math.sqrt(NUM); i++) {
+            if (!prime[i]) continue;
+
+            for (int j = i * i; j <= NUM; j += i) {
+                prime[j] = false;
+            }
         }
-        return true;
     }
-
-
 }
-
-
