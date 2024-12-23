1696a567b0c1aeb141e6d863b3a84cb6e2436c11
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-12-23 14:03:21 +0900

    [Silver IV] Title: 스택 2, Time: 932 ms, Memory: 235656 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -1,42 +1,52 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
         StringBuilder sb = new StringBuilder();
 
-        StringTokenizer st;
-        Stack<Integer> stack = new Stack<>();
-
         while (n-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-
-            if (a == 1) {
-                stack.push(Integer.parseInt(st.nextToken()));
-            } else if (a == 2) {
-                if (!stack.isEmpty()) sb.append(stack.pop()).append("\n");
-                else sb.append(-1).append("\n");
-            } else if (a == 3) sb.append(stack.size()).append("\n");
-
-            else if (a == 4) {
-                if (stack.isEmpty()) sb.append(1).append("\n");
-                else sb.append(0).append("\n");
+            String s = br.readLine();
+            int num;
+            if (s.length() > 1) {
+                StringTokenizer st = new StringTokenizer(s);
+                num = Integer.parseInt(st.nextToken());
+                int value = Integer.parseInt(st.nextToken());
+                stack.push(value);
+                continue;
+            }
+            num = Integer.parseInt(s);
+            if (num == 2) {
+                if (!stack.isEmpty()) {
+                    sb.append(stack.pop()).append("\n");
+                } else {
+                    sb.append(-1).append("\n");
+                }
+            } else if (num == 3) {
+                sb.append(stack.size()).append("\n");
+            } else if (num == 4) {
+                if (stack.isEmpty()) {
+                    sb.append(1).append("\n");
+                } else {
+                    sb.append(0).append("\n");
+                }
             } else {
-                if (!stack.isEmpty()) sb.append(stack.peek()).append("\n");
-                else sb.append(-1).append("\n");
+                if (!stack.isEmpty()) {
+                    sb.append(stack.peek()).append("\n");
+                } else {
+                    sb.append(-1).append("\n");
+                }
             }
         }
 
         System.out.println(sb);
     }
-
-
 }
-
-
