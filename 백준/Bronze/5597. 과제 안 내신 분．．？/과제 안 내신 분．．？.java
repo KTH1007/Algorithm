ef8b4129d859fbbc7017fb14920895998ef9cdf4
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-12-23 12:10:41 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,19 +1,20 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[31];
-        for (int i = 1; i <= 28; i++) {
+        boolean[] check = new boolean[30];
+
+        for (int i = 0; i < 28; i++) {
             int n = Integer.parseInt(br.readLine());
-            arr[n]++;
+            check[n - 1] = true;
         }
-        for (int i = 1; i <= 30; i++) {
-            if (arr[i] == 0) System.out.println(i);
+
+        for (int i = 0; i < 30; i++) {
+            if (!check[i]) System.out.println(i + 1);
         }
     }
-
-
 }
