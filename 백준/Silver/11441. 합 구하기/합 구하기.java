18079487711f4edd406afb55744fc07200554934
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-12-27 16:10:49 +0900

    [Silver III] Title: 합 구하기, Time: 540 ms, Memory: 55452 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i - 1] + arr[i];
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        while (m-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+
+            sb.append(dp[j] - dp[i - 1]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+}
