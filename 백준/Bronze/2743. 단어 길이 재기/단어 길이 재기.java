31f2a758551f494693e58568ca75928fb9f1080b
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-12-29 18:14:43 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -1,12 +1,11 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        System.out.println(s.length());
+        System.out.println(br.readLine().length());
     }
-
 }
