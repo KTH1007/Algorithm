ebbd8c900659a52f973194360b35c2831ae2451a
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-12-29 18:33:53 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -2,36 +2,27 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int n = Integer.parseInt(st.nextToken());	
-		for(int j=0; j<n; j++) {
-			String s = br.readLine();
-			Stack<Character> stack = new Stack<>();
-			for(int i=0; i<s.length(); i++) {
-				if(s.charAt(i) == '(') {
-					stack.push(s.charAt(i));
-				}
-				else if(s.charAt(i) == ')') {
-					if(stack.isEmpty()) {
-						stack.push(s.charAt(i));
-						break;
-					}
-					else
-						stack.pop();
-				}
-			}
-			if(stack.isEmpty())
-				sb.append("YES").append("\n");
-			else
-				sb.append("NO").append("\n");
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0) {
+            Stack<Character> stack = new Stack<>();
+            String s = br.readLine();
+
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c == '(') stack.push(c);
+                else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') stack.pop();
+                else if (c == ')') stack.push(c);
+            }
+            if (stack.isEmpty()) sb.append("YES").append("\n");
+            else sb.append("NO").append("\n");
+        }
+
+        System.out.println(sb);
+    }
 }
