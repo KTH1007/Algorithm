069090b2f080ea076d3efeb5e7c42190dd357ecb
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-12-29 19:36:36 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 120 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -1,45 +1,51 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int seq = 1;
 
         Stack<Integer> stack = new Stack<>();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
 
-        for (int i = n - 1; i >= 0; i--) stack.push(arr[i]);
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-        Stack<Integer> temp = new Stack<>();
 
-        int i = 1;
-        while (true) {
-            if (!stack.isEmpty() && stack.peek() == i) {
-                i++;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == seq) {
+                seq++;
+                continue;
+            }
+            if (!stack.isEmpty() && stack.peek() == seq) {
                 stack.pop();
-            } else if (!temp.isEmpty() && temp.peek() == i) {
-                i++;
-                temp.pop();
-            } else {
-                if (stack.isEmpty()) break;
-                else {
-                    temp.push(stack.pop());
-
-                }
+                seq++;
+                i--;
+            }
+            else {
+                stack.push(arr[i]);
             }
         }
 
-        if (i - 1 == n) System.out.println("Nice");
-        else System.out.println("Sad");
+        while (!stack.isEmpty()) {
+            if (stack.peek() != seq) {
+                System.out.println("Sad");
+                return;
+            } else {
+                stack.pop();
+                seq++;
+            }
+        }
+
+        System.out.println("Nice");
     }
-
-
 }
-
-
