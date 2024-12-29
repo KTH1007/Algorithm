e0ead2656cbdb69af7e486fc4bd42255ba0f4ca3
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-12-29 18:18:05 +0900

    [Bronze V] Title: 문자열, Time: 96 ms, Memory: 13980 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,18 +1,19 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
+
+        while (t-- > 0) {
             String s = br.readLine();
             sb.append(s.charAt(0)).append(s.charAt(s.length() - 1)).append("\n");
         }
+
         System.out.println(sb);
     }
-
-
 }
