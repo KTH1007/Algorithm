4790056fb46978aebd02d373baccea6161d47df1
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2024-12-29 18:38:55 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 228 ms, Memory: 19596 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -5,43 +5,28 @@ import java.util.Stack;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		//StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		//int n = Integer.parseInt(st.nextToken());	
-		while(true) {
-			String s = br.readLine();
-			if(s.charAt(s.length()-1) == '.' && s.length() == 1) {
-				break;
-			}
-			Stack<Character> stack = new Stack<>();
-			for(int i=0; i<s.length(); i++) {
-				if(s.charAt(i) == '(' || s.charAt(i) == '[') {
-					stack.push(s.charAt(i));
-				}
-				else if(s.charAt(i) == ')') {
-					if(stack.isEmpty() || stack.peek() == '[') {
-						stack.push(s.charAt(i));
-						break;
-					}
-					else
-						stack.pop();
-				}
-				else if(s.charAt(i) == ']') {
-					if(stack.isEmpty() || stack.peek() == '(') {
-						stack.push(s.charAt(i));
-						break;
-					}
-					else
-						stack.pop();
-				}
-			}
-			if(stack.isEmpty())
-				sb.append("yes").append("\n");
-			else
-				sb.append("no").append("\n");
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String s = br.readLine();
+            if (s.equals(".")) break;
+            Stack<Character> stack = new Stack<>();
+
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c == '(') stack.push(c);
+                else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') stack.pop();
+                else if (c == ')') stack.push(c);
+
+                if (c == '[') stack.push(c);
+                else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') stack.pop();
+                else if (c == ']') stack.push(c);
+            }
+            if (stack.isEmpty()) sb.append("yes").append("\n");
+            else sb.append("no").append("\n");
+        }
+        System.out.println(sb);
+    }
 }
