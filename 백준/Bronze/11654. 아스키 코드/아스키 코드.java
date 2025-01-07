6cfe25b2dc9fa8cc485d02875a4d2b1a19cebe92
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2025-01-07 15:14:45 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,20 +1,12 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String s = br.readLine();
-		char c = s.charAt(0);
-		bw.write(Integer.toString((int)c));
-		bw.flush();
-		bw.close();
-
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println((int) s.charAt(0));
+    }
 }
