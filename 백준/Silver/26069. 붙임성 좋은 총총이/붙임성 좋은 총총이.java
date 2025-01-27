be8c34b12f5b64fd40ea69049e1b79ceb51740fe
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2025-01-27 21:27:25 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 120 ms, Memory: 14912 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
@@ -1,29 +1,28 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashSet<String> set = new HashSet<>();
 
         int n = Integer.parseInt(br.readLine());
+        Set<String> set = new HashSet<>();
+        set.add("ChongChong");
         StringTokenizer st;
 
-        set.add("ChongChong");
         while (n-- > 0) {
             st = new StringTokenizer(br.readLine());
             String s1 = st.nextToken();
             String s2 = st.nextToken();
 
             if (set.contains(s1) || set.contains(s2)) {
-                if (set.contains(s1)) {
-                    set.add(s2);
-                } else {
-                    set.add(s1);
-                }
+                set.add(s1);
+                set.add(s2);
             }
         }
 
