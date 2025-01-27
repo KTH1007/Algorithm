4bdccc1984cdc22c2e6d1d49886cfeec50e18205
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2025-01-27 21:01:30 +0900

    [Bronze II] Title: 수 정렬하기, Time: 120 ms, Memory: 14572 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        List<Integer> list = new ArrayList<>();
+        while (n-- > 0) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        list.sort(null);
+        StringBuilder sb = new StringBuilder();
+
+        for (Integer i : list) {
+            sb.append(i).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
