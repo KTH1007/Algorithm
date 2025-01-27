87cca8a670bf8e308e81701ef4ef7012a4384240
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2025-01-27 20:57:28 +0900

    [Bronze II] Title: 대표값2, Time: 104 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.java
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -1,24 +1,24 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[5];
+
+        List<Integer> list = new ArrayList<>();
+
         int sum = 0;
-
         for (int i = 0; i < 5; i++) {
-            int n = Integer.parseInt(br.readLine());
-            sum += n;
-            arr[i] = n;
+            list.add(Integer.parseInt(br.readLine()));
+            sum += list.get(i);
         }
-        Arrays.sort(arr);
+
+        list.sort(null);
         System.out.println(sum / 5);
-        System.out.println(arr[2]);
+        System.out.println(list.get(2));
     }
-
-
 }
-
