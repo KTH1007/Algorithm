7317fccb38faa3cc9d6ae454293139a9d8d88fec
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2025-02-04 00:10:33 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 736 ms, Memory: 42756 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -2,41 +2,56 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        String[] arr = new String[n];
-        HashMap<String, Integer> map = new HashMap<>();
-        for(int i=0; i<n; i++){
-            String s = br.readLine();
-            if(s.length()<m)
-                continue;
-            Integer count = map.getOrDefault(s, 0);
-            map.put(s, count+1);
-        }
-        List<String> words = map.keySet().stream().collect(Collectors.toList());
-        words.sort((o1, o2) -> {
-            int c1=map.get(o1);
-            int c2=map.get(o2);
 
-            if(c1==c2){
-                if(o1.length() == o2.length()) {
-                    return o1.compareTo(o2); // 사전 순
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+
+            if (s.length() < m) continue;
+
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        List<Word> list = new ArrayList<>();
+        for (String s : map.keySet()) {
+            list.add(new Word(s, map.get(s)));
+        }
+
+        list.sort((o1, o2) -> {
+            if (o2.count == o1.count) {
+                if (o2.word.length() == o1.word.length()) {
+                    return o1.word.compareTo(o2.word);
                 }
-                return o2.length()-o1.length(); // 단어 길이
+                return o2.word.length() - o1.word.length();
             }
-            return c2-c1; // 자주 나오는 단어
+            return o2.count - o1.count;
         });
 
-        for(int i=0; i <words.size(); i++){
-            sb.append(words.get(i)).append("\n");
+        StringBuilder sb = new StringBuilder();
+
+        for (Word word : list) {
+            sb.append(word.word).append("\n");
         }
+
         System.out.println(sb);
+    }
+
+    private static class Word {
+        String word;
+        int count;
+
+        public Word(String word, int count) {
+            this.word = word;
+            this.count = count;
+        }
     }
 }
