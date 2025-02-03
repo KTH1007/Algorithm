ad709783e8c7345821e51350922087dadc3f8d26
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2025-02-03 17:56:08 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -1,15 +1,22 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
-        long sum = 1;
-        for (int i = 1; i <= n; i++) sum *= i;
-        System.out.println(sum);
+
+        long[] dp = new long[21];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = i * dp[i - 1];
+        }
+
+        System.out.println(dp[n]);
     }
-
-
 }
