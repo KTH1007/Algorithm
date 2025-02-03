ca6382fec431a65813ef47b577ab2bea9602732b
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2025-02-03 12:58:16 +0900

    [Bronze IV] Title: 모음의 개수, Time: 96 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.java
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.java
@@ -1,30 +1,29 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringBuilder sb = new StringBuilder();
-        int count = 0;
         while (true) {
             String s = br.readLine();
+
             if (s.equals("#")) break;
+
+            int count = 0;
             for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u' ||
-                        s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U') {
+                char c = s.charAt(i);
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
                     count++;
                 }
             }
+
             sb.append(count).append("\n");
-            count = 0;
         }
+
         System.out.println(sb);
-
     }
-
 }
-
-
-
-
