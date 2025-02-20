bcca1424cc154a93b45650df9305d13f90d904c8
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2025-02-20 20:17:04 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,25 +1,29 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    static int[] arr;
+    static int n;
+    static int m;
     static boolean[] visited;
+    static int[] arr;
     static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        arr = new int[n];
         visited = new boolean[n];
+        arr = new int[m];
+
         dfs(0, 0);
         System.out.println(sb);
     }
 
-    static void dfs(int depth, int at) {
+    private static void dfs(int depth, int num) {
         if (depth == m) {
             for (int i = 0; i < m; i++) {
                 sb.append(arr[i]).append(" ");
@@ -27,10 +31,13 @@ public class Main {
             sb.append("\n");
             return;
         }
-        for (int i = at; i < n; i++) {
+
+        for (int i = num; i < n; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
             arr[depth] = i + 1;
-            dfs(depth + 1, i + 1);
+            dfs(depth + 1, i);
+            visited[i] = false;
         }
     }
-
 }
