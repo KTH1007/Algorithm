e805516fd9dc153eba7aee38f2be12baebf83f05
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2025-02-20 13:31:29 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 108 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,49 +1,17 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'c' && i + 1 < s.length()) {
-                if (s.charAt(i + 1) == '=' || s.charAt(i + 1) == '-') {
-                    count++;
-                    i++;
-                    continue;
-                }
-            } else if (s.charAt(i) == 'd' && i + 1 < s.length()) {
-                if (s.charAt(i + 1) == '-') {
-                    count++;
-                    i++;
-                    continue;
-                }
-                if (s.charAt(i + 1) == 'z' && i + 2 < s.length()) {
-                    if (s.charAt(i + 2) == '=') {
-                        count++;
-                        i += 2;
-                        continue;
-                    }
-                }
-            } else if ((s.charAt(i) == 'l' && i + 1 < s.length()) || (s.charAt(i) == 'n' && i + 1 < s.length())) {
-                if (s.charAt(i + 1) == 'j') {
-                    count++;
-                    i++;
-                    continue;
-                }
-            } else if ((s.charAt(i) == 's' && i + 1 < s.length()) || (s.charAt(i) == 'z' && i + 1 < s.length())) {
-                if (s.charAt(i + 1) == '=') {
-                    count++;
-                    i++;
-                    continue;
-                }
-            }
-            count++;
-        }
-        System.out.println(count);
+
+        s = s.replace("c=", "!").replace("c-", "!").replace("dz=", "!")
+                .replace("d-", "!").replace("lj", "!").replace("nj", "!")
+                .replace("s=", "!").replace("z=", "!");
+
+        System.out.println(s.length());
     }
-
-
 }
