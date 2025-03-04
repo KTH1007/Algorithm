4f86dcdb8b950262e87b28f4543066ed6b89c547
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2025-03-04 23:27:36 +0900

    [Silver I] Title: 스타트와 링크, Time: 348 ms, Memory: 18248 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,51 +1,54 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
     static int n;
-    static int[][] map;
+    static int[][] arr;
     static boolean[] visited;
-    static int min = Integer.MAX_VALUE;
+    static int ability = Integer.MAX_VALUE;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        map = new int[n][n];
+        arr = new int[n][n];
         visited = new boolean[n];
+
+        StringTokenizer st;
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+
         dfs(0, 0);
-        System.out.println(min);
+        System.out.println(ability);
+
     }
 
-    static void dfs(int depth, int index) {
+    private static void dfs(int depth, int at) {
         if (depth == n / 2) {
-            diff();
+            int start = 0;
+            int link = 0;
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (visited[i] && visited[j]) start += arr[i][j] + arr[j][i];
+                    else if (!visited[i] && !visited[j]) link += arr[i][j] + arr[j][i];
+                }
+            }
+            if (Math.abs(start - link) < ability) {
+                ability = Math.abs(start - link);
+            }
             return;
         }
-        for (int i = index; i < n; i++) {
+
+        for (int i = at; i < n; i++) {
             if (visited[i]) continue;
             visited[i] = true;
             dfs(depth + 1, i + 1);
             visited[i] = false;
         }
     }
-
-    static void diff() {
-        int start = 0;
-        int link = 0;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (visited[i] && visited[j]) start += map[i][j] + map[j][i];
-                else if (!visited[i] && !visited[j]) link += map[i][j] + map[j][i];
-            }
-        }
-        min = Math.min(min, Math.abs(start - link));
-    }
-
 }
