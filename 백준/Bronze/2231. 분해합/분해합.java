a21ff78d3bb124d9b89aaadc7cc5e94b5e3dd92d
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2025-03-10 15:45:50 +0900

    [Bronze II] Title: 분해합, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            sum = 0;
+            int num = i;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            if (i + sum == n) {
+                result = i;
+                break;
+            }
+        }
+
+        System.out.println(result);
+
+    }
+}
