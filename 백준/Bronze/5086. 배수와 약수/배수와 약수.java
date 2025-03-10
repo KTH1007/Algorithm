402ec217ce3caa5c00e390dd6b378d218105a92f
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2025-03-10 14:44:43 +0900

    [Bronze III] Title: 배수와 약수, Time: 96 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -1,30 +1,29 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
         while (true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            if (n == 0 && m == 0) break;
-            if (n > m) {
-                if (n % m == 0) {
-                    System.out.println("multiple");
-                    continue;
-                }
-            } else {
-                if (m % n == 0) {
-                    System.out.println("factor");
-                    continue;
-                }
-            }
-            System.out.println("neither");
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0) break;
+
+            if (b % a == 0) sb.append("factor").append("\n");
+            else if (a % b == 0) sb.append("multiple").append("\n");
+            else sb.append("neither").append("\n");
         }
 
+        System.out.println(sb);
     }
-
 }
