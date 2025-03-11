053b43627b241546a67e0b604f442d25f4420128
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2025-03-11 17:47:29 +0900

    [level 2] Title: 전화번호 목록, Time: 318.36 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,12 +4,16 @@ class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         
+        // 문자열 특성을 이용하기 위해 정렬
         Arrays.sort(phone_book);
+        
+        // 해당 문자열이 접두어인 경우 그 뒤에 접두어를 사용하는 문자열이 오게 됨
         for (int i = 0; i < phone_book.length - 1; i++) {
             if (phone_book[i + 1].startsWith(phone_book[i])) {
-                return false;
+                answer = false;
             }
         }
+        
         return answer;
     }
 }
