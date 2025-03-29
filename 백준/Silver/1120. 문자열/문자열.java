8c4036ac24965e2b48cb6a571444a0e33c6a47c8
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2025-03-29 12:01:22 +0900

    [Silver IV] Title: 문자열, Time: 104 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Silver/1120. 문자열/문자열.java
+++ 백준/Silver/1120. 문자열/문자열.java
@@ -1,48 +1,41 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String A = st.nextToken();
-        String B = st.nextToken();
-        int min = Integer.MAX_VALUE;
-        int n = B.length() - A.length();
-        int i = 0;
+        String x = st.nextToken();
+        String y = st.nextToken();
 
-        if (A.length() == B.length()) {
-            min = Math.min(min, compare(A, B));
-        } else if (n == 1) {
-            min = Math.min(min, compare(B.charAt(0) + A, B));
-            min = Math.min(min, compare(A + B.charAt(B.length() - 1), B));
-        } else {
-            min = Math.min(min, compare(B.substring(0, n) + A, B));
-            min = Math.min(min, compare(A + B.substring(A.length() + 1, B.length()), B));
+        int min = Integer.MAX_VALUE;
+        if (y.contains(x)) {
+            System.out.println(0);
+            return;
+        }
+        int count = 0;
+        for (int i = 0; i < x.length(); i++) {
+            if (x.charAt(i) != y.charAt(i)) count++;
         }
 
-        // A 문자열이 중간에 위치한 경우
-        while (n > 0) {
-            String tempA = B.substring(0, i) + A;
-            String temp = tempA + B.substring(tempA.length(), tempA.length() + n);
-            min = Math.min(min, compare(temp, B));
-            i++;
-            n--;
+        min = Math.min(min, count);
+
+        int left = 0;
+        int right = y.length() - x.length();
+
+        while (left <= right) {
+            count = 0;
+            for (int i = 0; i < x.length(); i++) {
+                if (x.charAt(i) != y.charAt(left + i)) count++;
+            }
+            min = Math.min(min, count);
+            left++;
         }
 
         System.out.println(min);
-    }
-
-    private static int compare(String A, String B) {
-        int count = 0;
-        for (int i = 0; i < A.length(); i++) {
-            if (A.charAt(i) != B.charAt(i)) {
-                count++;
-            }
-        }
-        return count;
     }
 }
