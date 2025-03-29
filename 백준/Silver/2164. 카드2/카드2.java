93172bfd25083b0385b4fdbe1099362b736a67ef
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2025-03-29 11:32:36 +0900

    [Silver IV] Title: 카드2, Time: 172 ms, Memory: 28868 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,27 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st =new StringTokenizer(br.readLine(), " ");
-		int n = Integer.parseInt(st.nextToken());
-		Queue<Integer> q = new LinkedList<>();
-		for(int i=1; i<=n; i++) {
-			q.offer(i);
-		}
-		while(q.size() != 1) {
-			q.remove();
-			q.offer(q.peek());
-			q.remove();
-		}
-		sb.append(q.poll());
-		System.out.println(sb);
-	}
+        int n = Integer.parseInt(br.readLine());
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int i = 1; i <= n; i++) {
+            deque.add(i);
+        }
+
+        while (deque.size() != 1) {
+            deque.poll();
+            deque.add(deque.poll());
+        }
+
+        System.out.println(deque.poll());
+    }
 }
