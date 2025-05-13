4d602f12c0c11a621897eb4ffb5325b6f4cd8334
TAE HEON KIM <104629952+KTH1007@users.noreply.github.com>
2025-05-13 19:00:02 +0900

    [level 2] Title: 모음 사전, Time: 44.41 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 0.0 MB, 시간: 0.00 ms
+메모리: 106 MB, 시간: 44.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 24일 10:16:37
+2025년 05월 13일 18:59:57
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,21 +1,32 @@
 import java.util.*;
 
 class Solution {
-    static String[] arr = {"A", "E", "I", "O", "U"};
-    static List<String> list = new ArrayList<>();
+    // 문자열을 담을 결과 리스트
+    static List<String> result = new ArrayList<>();
     public int solution(String word) {
-        int answer = 0;
-        dfs(0, "");
-        answer = list.indexOf(word);
+        // 모음 배열 생성
+        String[] alphabet = {"A", "E", "I", "O", "U"};
+        
+        dfs(0, new ArrayList<>(), alphabet);
+        
+        int answer = result.indexOf(word);
         return answer;
     }
     
-    private static void dfs(int depth, String current) {
-        list.add(current);
-        if (depth == 5) return;
-        for (int i = 0; i < 5; i++) {
-            System.out.println(current + arr[i] + " : " + depth);
-            dfs(depth + 1, current + arr[i]);
+    private static void dfs(int depth, List<String> selected, String[] alphabet) {
+        // 결과 리스트에 삽입
+        result.add(selected.toString().replaceAll("[^A-Z]", ""));
+        
+        if (depth == 5) {
+            return;
         }
+        
+        // 재귀호출
+        for (String alpha : alphabet) {
+            selected.add(alpha);
+            dfs(depth + 1, selected, alphabet);
+            selected.remove(selected.size() - 1);
+        }
+        
     }
 }
