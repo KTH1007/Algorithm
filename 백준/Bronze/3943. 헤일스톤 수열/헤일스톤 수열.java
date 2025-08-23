03d5780c7032e7e9091356d60a809a0f290ebf48
TAEHEON KIM <104629952+KTH1007@users.noreply.github.com>
2025-08-24 08:04:35 +0900

    [Bronze II] Title: 헤일스톤 수열, Time: 380 ms, Memory: 27460 KB -BaekjoonHub

---
+++ 백준/Bronze/3943. 헤일스톤 수열/헤일스톤 수열.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    int T = Integer.parseInt(br.readLine());
+    StringBuilder sb = new StringBuilder();
+    while (T-- > 0) {
+      int N = Integer.parseInt(br.readLine());
+      int ans = N;
+      while (true) {
+        ans = Math.max(ans, N);
+
+        if ((N & (-N)) == N) {
+          break;
+        }
+
+        if (N % 2 == 0) {
+          N /= 2;
+        } else {
+          N = N * 3 + 1;
+        }
+      }
+      sb.append(ans).append("\n");
+    }
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
