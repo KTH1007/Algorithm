1ccb2c98e9a43593d9c7545014242d96b0d9caba
TAEHEON KIM <104629952+KTH1007@users.noreply.github.com>
2025-09-13 11:14:58 +0900

    [Silver V] Title: K-세준수, Time: 224 ms, Memory: 14772 KB -BaekjoonHub

--- 백준/Silver/1418. K－세준수/K－세준수.java
+++ 백준/Silver/1418. K－세준수/K－세준수.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Main {
 
@@ -12,26 +11,40 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int k = Integer.parseInt(br.readLine());
 
+        Boolean[] prime = new Boolean[n + 1];
+        Arrays.fill(prime, true);
+        isPrime(prime, n);
+
         int count = 0;
         for (int i = 2; i <= n; i++) {
-            if (count(i, k)) count++;
+            if (count(prime, i, k)) count++;
         }
 
         System.out.println(count + 1);
     }
 
-    private static boolean count(int n, int k) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 2; i <= n; i++) {
-            while (n % i == 0) {
-                list.add(i);
-                n /= i;
+    private static void isPrime(Boolean[] prime, int n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            for (int j = i * i; j <= n ; j += i) {
+                prime[j] = false;
+            }
+        }
+    }
+
+    private static boolean count(Boolean[] prime, int num, int k) {
+        int max = 0;
+        for (int i = 1; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                if (prime[i]) {
+                    max = Math.max(max, i);
+                }
+                if (prime[num / i]) {
+                    max = Math.max(max, num / i);
+                }
             }
         }
 
-        list.sort((o1, o2) -> o1 - o2);
-        if (list.isEmpty()) return false;
-        if (list.get(list.size() - 1) <= k) return true;
+        if (max <= k) return true;
         return false;
     }
 }
