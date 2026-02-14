93931df073d5a1190c234c22536cd2f95369e46c
TAEHEON KIM <104629952+KTH1007@users.noreply.github.com>
2026-02-14 12:57:04 +0900

    [Gold IV] Title: 키 순서, Time: 624 ms, Memory: 46020 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -1,15 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
 
 public class Main {
     static int n, m;
-    static List<Integer>[] bigger;
-    static List<Integer>[] smaller;
+    static boolean[][] dist;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,12 +13,9 @@ public class Main {
         n = Integer.parseInt(tokens[0]);
         m = Integer.parseInt(tokens[1]);
 
-        bigger = new List[n + 1];
-        smaller = new List[n + 1];
-
-        for (int i = 0; i <= n; i++) {
-            bigger[i] = new ArrayList<>();
-            smaller[i] = new ArrayList<>();
+        dist = new boolean[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            dist[i][i] = true;
         }
 
         for (int i = 0; i < m; i++) {
@@ -31,57 +23,40 @@ public class Main {
             int a = Integer.parseInt(tokens[0]);
             int b = Integer.parseInt(tokens[1]);
 
-            smaller[a].add(b);
-            bigger[b].add(a);
+            dist[a][b] = true;
         }
 
+        floyd();
         System.out.println(studentCheck());
     }
 
-    private static int studentCheck() {
-        int count = 0;
-        for (int i = 1; i <= n; i++) {
-            if (canKnowRank(i)) {
-                count++;
-            }
-        }
-
-        return count;
-    }
-
-    private static boolean canKnowRank(int student) {
-        int tallerCount = bfs(student, smaller);
-        int shorterCount = bfs(student, bigger);
-
-        if (tallerCount + shorterCount == n - 1) {
-            return true;
-        }
-
-        return false;
-    }
-
-    private static int bfs(int start, List<Integer>[] edges) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        boolean[] visited = new boolean[n + 1];
-
-        queue.add(start);
-        visited[start] = true;
-        int count = 0;
-
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-            for (int i = 0; i < edges[node].size(); i++) {
-                int next = edges[node].get(i);
-                if (visited[next]) {
-                    continue;
+    private static void floyd() {
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (dist[i][k] && dist[k][j]) {
+                        dist[i][j] = true;
+                    }
                 }
+            }
+        }
+    }
 
-                visited[next] = true;
-                queue.add(next);
-                count++;
+    private static int studentCheck() {
+        int totalCount = 0;
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][j] || dist[j][i]) {
+                    count++;
+                }
+            }
+
+            if (count == n) {
+                totalCount++;
             }
         }
 
-        return count;
+        return totalCount;
     }
 }
