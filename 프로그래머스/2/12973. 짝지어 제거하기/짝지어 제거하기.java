268d258e7ff8bd6c8b4945c2c7ab179f41e3c4f4
TAEHEON KIM <104629952+KTH1007@users.noreply.github.com>
2026-03-16 14:19:12 +0900

    [level 2] Title: 짝지어 제거하기, Time: 57.31 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -1,23 +1,33 @@
 import java.util.*;
+
 class Solution
 {
     public int solution(String s)
     {
-        int answer = -1;
-        Stack<Character> stack = new Stack<>();
+
+        if (remove(s).length() == 0) {
+            return 1;
+        }
+        
+        return 0;
+    }
+    
+    private String remove(String s) {
+        StringBuilder sb = new StringBuilder();
+        Deque<Character> stack = new ArrayDeque<>();
+        
         for (int i = 0; i < s.length(); i++) {
-            if(stack.isEmpty()) stack.push(s.charAt(i));
-            else {
-                if(stack.peek() == s.charAt(i)){
-                    stack.pop();
-                } else {
-                    stack.push(s.charAt(i));
-                }
+            if (!stack.isEmpty() && stack.peek() == s.charAt(i)) {
+                stack.pop();
+            } else {
+                stack.push(s.charAt(i));
             }
         }
-        if(stack.isEmpty()) answer = 1;
-        else answer = 0;
-
-        return answer;
+        
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        sb.reverse();
+        return sb.toString();
     }
 }
