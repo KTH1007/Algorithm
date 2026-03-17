718fa0a1e5b9fee2b21ac197e6443fb75c9f226d
TAEHEON KIM <104629952+KTH1007@users.noreply.github.com>
2026-03-17 15:09:21 +0900

    [level 2] Title: 카펫, Time: 8.11 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,18 +1,25 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        int sum = brown + yellow;
-        for (int i = 3; i < sum; i++) {
-            if (sum % i == 0) {
-                int w = i;
-                int h = sum / i;
-                
-                if ((h - 2) * (w - 2) == yellow) {
-                    answer[0] = w;
-                    answer[1] = h;
+        int[] answer = {};
+        
+        // 가로 * 세로 == brwon + yellow
+        
+        // 가로 * 2 + 세로 * 2 - 4 => brown
+        
+        // yellow = 가로 * 세로 - brwon
+        
+        for (int col = 1; col <= brown + yellow; col++) {
+            for (int row = 1; row <= brown; row++) {
+                if (row * col == brown + yellow) {
+                    int a = row * 2 + col * 2 - 4;
+                    int b = row * col - a;
+                    if (a == brown && b == yellow) {
+                        return new int[]{row, col};
+                    }
                 }
             }
         }
+
         return answer;
     }
 }
