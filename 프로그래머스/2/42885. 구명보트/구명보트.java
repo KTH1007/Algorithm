1691c106e1e48682b8dfa158d94e276c21ec074f
TAEHEON KIM <104629952+KTH1007@users.noreply.github.com>
2026-03-18 21:29:06 +0900

    [level 2] Title: 구명보트, Time: 10.70 ms, Memory: 56.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,18 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0;
-        
         Arrays.sort(people);
-        
-        int k = 0;
-        
-        for (int i = people.length - 1; i >= k; i--) {
-            if (people[i] + people[k] <= limit) {
-                k++;
+        // 투포인터?
+    
+        return twoPointer(people, limit);
+    }
+    
+    private int twoPointer(int[] people, int limit) {
+        int left = 0;
+        int right = people.length - 1;
+        int count = 0;
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                count++;
+                left++;
+                right--;
+            } else {
+                count++;
+                right--;
             }
-            answer++;
         }
-        return answer;
+        
+        return count;
     }
 }
