0fa3ff218c582950ea388c486efcbe4b94530a66
TAEHEON KIM <104629952+KTH1007@users.noreply.github.com>
2026-03-22 19:08:44 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 147.98 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -3,14 +3,23 @@ import java.util.*;
 class Solution {
     public int solution(int[] elements) {
         int answer = 0;
-        HashSet<Integer> set = new HashSet<>();
+        
+        // 현재 포인터부터 n개 선택
+        // ex) 2일때 4개 선택 2 3 4 0, len = 5
+        // ++, if (i == len - 1) i = 0; 
+        Set<Integer> set = new HashSet<>();
         
         for (int i = 0; i < elements.length; i++) {
-            int sum = 0;
-            for (int j = 0; j < elements.length; j++) {
-                sum += elements[(i + j) % elements.length];
+            int sum = elements[i];
+            set.add(sum);
+            // System.out.println(sum);
+            for (int j = 1; j < elements.length; j++) {
+                int position = (i + j) % elements.length;
+                sum += elements[position];
+                // System.out.println(sum);
                 set.add(sum);
             }
+            // System.out.println();
         }
         
         answer = set.size();
