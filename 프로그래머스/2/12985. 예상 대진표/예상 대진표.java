a23603ef70f64ad1bf5f07104a965635e3425d88
TAEHEON KIM <104629952+KTH1007@users.noreply.github.com>
2026-03-24 17:38:49 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,31 +1,22 @@
+import java.util.*;
+
 class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int answer = 1;
-        int temp = 0;
-        if (a > b) {
-            temp = a;
-            a = b;
-            b = temp;
-        }
-        while (true) {
-            if (Math.abs(a - b) == 1 && (a % 2 == 1 && b % 2 == 0))  break;
-            
-            if (a % 2 == 0) a /= 2;
-            else a = a / 2 + 1;
-            
-            if (b % 2 == 0) b /= 2;
-            else b = b / 2 + 1;
-            
+        int answer = 0;
+
+        // a 3 b = 8
+        // 1 2 3 4 5 6 7 8
+        // 1 3 5 8
+        // 3 8
+        
+        // n이 계속 절반씩 낮아짐
+        while (a != b) {
+            a = a / 2 + a % 2;
+            b = b / 2 + b % 2;
             answer++;
         }
         return answer;
     }
 }
-
-/* 
-1 2   3  4    5  6   7  8
-1  2   3  4
-1 2
-*/
