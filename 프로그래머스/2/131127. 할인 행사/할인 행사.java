f39c5a390b1952d816346dcabbc09601ca5d7469
TAEHEON KIM <104629952+KTH1007@users.noreply.github.com>
2026-03-25 13:25:50 +0900

    [level 2] Title: 할인 행사, Time: 67.33 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,34 +3,65 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        HashMap<String, Integer> map = new HashMap<>();
         
+        Map<String, Integer> wantProductAndCount = new HashMap<>();
         for (int i = 0; i < want.length; i++) {
-            map.put(want[i], number[i]);
+            wantProductAndCount.put(want[i], number[i]);
         }
-        
-        
-        for (int i = 0; i <= discount.length - 10; i++) {
-            HashMap<String, Integer> temp = new HashMap<>();
-            for (int j = i; j < i + 10; j++) {
-                temp.put(discount[j], temp.getOrDefault(discount[j], 0) + 1);
-            }
-            
-            boolean flag = true;
-            
-            for (int j = 0; j < want.length; j++) {
-                if (!temp.containsKey(want[j])) {
-                    flag = false;
-                    break;
-                }
-                if (map.get(want[j]) != temp.get(want[j])) {
-                    flag = false;
-                    break;
-                }
-                System.out.println("dd");
-            }
-            if (flag) answer++;
-        }
+
+        answer = slidingWindow(wantProductAndCount, discount);
         return answer;
+    }
+    
+    private static int slidingWindow(Map<String, Integer> wantProductAndCount, String[] discount) {
+
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < 10; i++) {
+            if (wantProductAndCount.containsKey(discount[i])) {
+                map.put(discount[i], map.getOrDefault(discount[i], 0) + 1);
+            }
+        }
+        
+        int count = 0;
+
+        if (checkCount(wantProductAndCount, map)) {
+            count++;
+        }
+        
+        int day = 0;
+        int right = 10;
+        while(right < discount.length) {
+            if (wantProductAndCount.containsKey(discount[day])) {
+                map.put(discount[day], map.get(discount[day]) - 1);
+                if (map.get(discount[day]) == 0) {
+                    map.remove(discount[day]);
+                }
+            }
+            
+            if (wantProductAndCount.containsKey(discount[right])) {
+                map.put(discount[right], map.getOrDefault(discount[right], 0) + 1);
+            }
+            
+            if (checkCount(wantProductAndCount, map)) {
+                count++;
+            }
+            
+            day++;
+            right++;
+        }
+        
+        return count;
+    }
+    
+    private static boolean checkCount(Map<String, Integer> wantProductAndCount, Map<String, Integer> map) {
+        
+        for (String key : wantProductAndCount.keySet()) {
+            if (!wantProductAndCount.get(key).equals(map.getOrDefault(key, 0))) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
