614cc53f0425deaae5cb1c65aed5b0ad43d096e9
TAEHEON KIM <104629952+KTH1007@users.noreply.github.com>
2026-03-30 12:53:31 +0900

    [level 2] Title: H-Index, Time: 6.39 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -3,12 +3,33 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        Arrays.sort(citations);
         
-        for (int i = 0; i < citations.length; i++) {
-            int temp = citations.length - i;
-            answer = Math.max(answer, Math.min(temp, citations[i]));
+        Arrays.sort(citations);
+        // 10 11 -> 2
+        // 카운트를 증가
+        // 카운트보다 큰 배열 크기 확인
+        // 카운트가 배열 크기보다 커지면 끝
+        
+        int count = 0;
+        while (count <= citations.length - 1) {
+            count++;
+            if (numCheck(count, citations)) {
+                answer = count;
+            }
         }
         return answer;
+    }
+    
+    private static boolean numCheck(int count, int[] citations) {
+        for (int i = 0; i < citations.length; i++) {
+            if (count <= citations[i]) {
+                int length = citations.length - i;
+                if (count == length) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
     }
 }
