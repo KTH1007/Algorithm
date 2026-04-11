0445362593040209e99ed8ef0a3cb2c91ec5e37a
TAEHEON KIM <104629952+KTH1007@users.noreply.github.com>
2026-04-11 12:31:25 +0900

    [level 2] Title: 타겟 넘버, Time: 4.22 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,19 +1,31 @@
 import java.util.*;
+
 class Solution {
-    static int answer = 0;
+    static boolean[] visited;
+    static int count;
+    
     public int solution(int[] numbers, int target) {
-        boolean[] visited = new boolean[numbers.length];
-        dfs(0, 0, numbers, target);
+        int answer = 0;
+        visited = new boolean[numbers.length];
+        // dfs로 number 맞춰가기, target이면 카운트증가
+        // 방문 체크 필요
+        // dfs 돌릴 때 현재값 더하거나 빼거나
+        
+        dfs(0, numbers, target, 0);
+        answer = count;
         return answer;
     }
-    void dfs(int depth, int sum, int[] numbers, int target) {
+    
+    private static void dfs(int depth, int[] numbers, int target, int num) {
         if (depth == numbers.length) {
-            if (sum == target) {
-                answer++;
+            if (num == target) {
+                count++;
             }
             return;
         }
-        dfs(depth + 1, sum + numbers[depth], numbers, target);
-        dfs(depth + 1, sum - numbers[depth], numbers, target);
+        
+        dfs(depth + 1, numbers, target, num + numbers[depth]);
+        dfs(depth + 1, numbers, target, num - numbers[depth]);
+        
     }
 }
