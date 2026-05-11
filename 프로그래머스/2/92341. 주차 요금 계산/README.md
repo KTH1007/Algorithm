11eddc3f3280b74f1df2586173e0f829b1f7c9a7
TAEHEON KIM <104629952+KTH1007@users.noreply.github.com>
2026-05-11 22:12:46 +0900

    [level 2] Title: 주차 요금 계산, Time: 5.02 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/README.md
+++ 프로그래머스/2/92341. 주차 요금 계산/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주차 요금 계산 - 92341 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341) 
 
 ### 성능 요약
 
-메모리: 67.4 MB, 시간: 0.39 ms
+메모리: 62.2 MB, 시간: 5.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 08일 09:59:42
+2026년 05월 11일 22:12:42
 
 ### 문제 설명
 
--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,48 +1,86 @@
 import java.util.*;
 
+// 입차 출차 기록 -> 차량별 요금 계산
+// 기본 시간 - 기본 요금
+// 추가 시간당 추가 요금 ((주차 시간 - 기본 시간) / 단위 시간) * 단위 요금 -> 기본시간 넘은 경우만
+// 주차 시간은 숫자로 변환
+// 출차 기록이 없으면 23:59 출차
+// 차량별 누적 요금
+// 차량 번호가 작은 자동차부터 -> List로 정렬
+// 출차 체크해야 됨 map 에 보관
+
+// 시간을 다 넣어두고 한 번에 계산
+
+
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-        StringTokenizer st;
-        HashMap<String, Integer> map = new HashMap<>();
-        HashMap<String, Integer> result = new HashMap<>();
-        List<String> carList = new ArrayList<>();
+        // 자동차별 누적 시간
+        Map<Integer, Integer> carTimes = new HashMap<>();
+        // 차량번호, 입차시간
+        Map<Integer, Integer> cars = new HashMap<>();
         
-        for (int i = 0; i < records.length; i++) {
-            String[] arr = records[i].split(" ");
-            String s = arr[0];
-            int time = (Integer.parseInt(s.substring(0,2)) * 60) + Integer.parseInt(s.substring(3,5));
-            String num = arr[1];
+        for (String record : records) {
+            String[] info = record.split(" ");
+            int time = changeTime(info[0]);
+            int carNumber = Integer.parseInt(info[1]);
+            String type = info[2];
             
-            if (!carList.contains(num)) {
-                carList.add(num);
-            }
-            
-            String inOut = arr[2];
-            
-            if (inOut.equals("IN")) {
-                map.put(num, time);
-            } else if (inOut.equals("OUT")) {
-                result.put(num, result.getOrDefault(num, 0) + time - map.get(num));
-                map.remove(num);
-            }
-        }
-        
-        int endTime = 23 * 60 + 59;
-        for (String key : map.keySet()) {
-            result.put(key, result.getOrDefault(key, 0) + endTime - map.get(key));
-        }
-        
-        Collections.sort(carList);
-        
-        int[] answer = new int[carList.size()];
-        
-        for (int i = 0; i < carList.size(); i++) {
-            if (result.get(carList.get(i)) <= fees[0]) {
-                answer[i] = fees[1];
+            if (type.equals("IN")) {
+                cars.put(carNumber, time);
             } else {
-                answer[i] = fees[1] + (int) Math.ceil(((double) (result.get(carList.get(i))) - fees[0]) / fees[2]) * fees[3];
+                int diffTime = time - cars.get(carNumber);
+                
+                carTimes.put(carNumber, carTimes.getOrDefault(carNumber, 0) + diffTime);
+                cars.remove(carNumber);
             }
+            
         }
+        
+        int lastTime = changeTime("23:59");
+        
+        // 출차 안 한 차량들 계산
+        for (int carNumber : cars.keySet()) {
+            int diffTime = lastTime - cars.get(carNumber);
+            carTimes.put(carNumber, carTimes.getOrDefault(carNumber, 0) + diffTime); 
+        }
+        
+        // 요금 계산
+        List<int[]> feeList = new ArrayList<>();
+        for (int car : carTimes.keySet()) {
+            int carFee = calcurateFees(carTimes.get(car), fees[0], fees[1], fees[2], fees[3]);
+            feeList.add(new int[]{car, carFee});
+        }
+        
+        int[] answer = new int[feeList.size()];
+        feeList.sort((o1, o2) -> o1[0] - o2[0]);
+        
+        for (int i = 0; i < feeList.size(); i++) {
+            answer[i] = feeList.get(i)[1];
+        }
+        
         return answer;
+    }
+    
+    private static int changeTime(String time) {
+        String[] times = time.split(":");
+        int hour = Integer.parseInt(times[0]) * 60;
+        int minute = Integer.parseInt(times[1]);
+        return hour + minute;
+    }
+    
+    private static int calcurateFees(int time, int baseTime, int baseFee, int partTime, int partFee) {
+        if (time <= baseTime) {
+            return baseFee;
+        }
+        
+        // 334 - 180 -> 154
+        int newTime = time - baseTime;
+        
+        int newFee = (newTime / partTime) * partFee;
+        if (newTime % partTime != 0) {
+            newFee += partFee;
+        }
+        
+        return baseFee + newFee;
     }
 }
