24337e92fbfe510f72e815a8ce5b3fff56d233f8
TAEHEON KIM <104629952+KTH1007@users.noreply.github.com>
2026-05-22 21:09:38 +0900

    [level 3] Title: 단속카메라, Time: 4.34 ms, Memory: 61.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 0;
+        Arrays.sort(routes, (o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
+            return o1[1] - o2[1];
+        });
+        
+        // 카메라는 end쪽에 두기
+        
+        answer++;
+        int camera = routes[0][1];
+        
+        for (int i = 1; i < routes.length; i++) {
+            int start = routes[i][0];
+            int end = routes[i][1];
+            
+            if (start > camera) {
+                answer++;
+                camera = end;
+            }
+        }
+      
+        return answer;
+    }
+}
